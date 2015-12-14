0b9d1c4907a265a4ed882cb3cb1a4d38d3532c17
Administrator <sugiyama@masato.nagano.jp>
2015-12-14 15:10:45 +0900

    [modify]文言の変更

--- sns.php
+++ sns.php
@@ -6,6 +6,6 @@
 <div id="socialbox">
     <a href="http://www.facebook.com/sharer.php?src=bm&u=<?php echo $url_encode;?>&t=<?php echo $title_encode;?>" class="social_item mm-facebook mm-ripple" title=""><i class="ion-social-facebook"></i>&nbsp;FaceBookでシェア
     </a>
-    <a href="http://twitter.com/intent/tweet?url=<?php echo $url_encode ?>&text=<?php echo $title_encode ?>&tw_p=tweetbutton" class="social_item ion-social-twitter mm-twitter mm-ripple">&nbsp;Twitterでシェア</a>
+    <a href="http://twitter.com/intent/tweet?url=<?php echo $url_encode ?>&text=<?php echo $title_encode ?>&tw_p=tweetbutton" class="social_item ion-social-twitter mm-twitter mm-ripple">&nbsp;Twitterでツイート</a>
 </div>
 <div class="clearfix"></div>
