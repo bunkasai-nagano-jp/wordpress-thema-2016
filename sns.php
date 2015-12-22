95ae30648cb255c130d61f2b6e33e830c0636c31
Administrator <sugiyama@masato.nagano.jp>
2015-12-22 19:03:22 +0900

    [modify]SNSボタンの修正
    style.cssのクラス名変更。sns.phpの修正。

--- sns.php
+++ sns.php
@@ -1,11 +1,10 @@
- <?php
+<?php
     $url_encode=urlencode(get_permalink());
     $title_encode=urlencode(get_the_title());
 ?>
 
 <div id="socialbox">
-    <a href="http://www.facebook.com/sharer.php?src=bm&u=<?php echo $url_encode;?>&t=<?php echo $title_encode;?>" class="social_item mm-facebook mm-ripple" title=""><i class="ion-social-facebook"></i>&nbsp;FaceBookでシェア
-    </a>
-    <a href="http://twitter.com/intent/tweet?url=<?php echo $url_encode ?>&text=<?php echo $title_encode ?>&tw_p=tweetbutton" class="social_item ion-social-twitter mm-twitter mm-ripple">&nbsp;Twitterでツイート</a>
+    <a href="http://www.facebook.com/sharer.php?src=bm&u=<?php echo $url_encode;?>&t=<?php echo $title_encode;?>" class="social-item social-facebook"><i class="ion-social-facebook"></i>&nbsp;FaceBookでシェア</a>
+    <a href="http://twitter.com/intent/tweet?url=<?php echo $url_encode ?>&text=<?php echo $title_encode ?>&tw_p=tweetbutton" class="social-item social-twitter"><i class="ion-social-twitter"></i>&nbsp;Twitterでツイート</a>
 </div>
 <div class="clearfix"></div>
