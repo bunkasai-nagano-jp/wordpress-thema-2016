c84e06502640861fe39ec1208b87900511230d4f
sugiyama <sugiyama@masato.nagano.jp>
2016-01-13 17:36:31 +0900

    [modify]アドセンス削除

--- sidebar.php
+++ sidebar.php
@@ -2,28 +2,10 @@
 <aside>
 <?php if (is_404()) { ?>
 <?php } else { ?>
-  <div class="ad">
-    <?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar(4) ) : else : //アドセンス ?>
-    <?php endif; ?>
-  </div>
 <?php } ?>
   <div id="mybox">
-    <?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar(1) ) : else : //サイドウイジェット読み込み ?>
+    <?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar(1) ) : else : ?>
     <?php endif; ?>
-  </div>
-  <!-- スマホだけのアドセンス -->
-<?php if (is_404()) { ?>
-<?php } else { ?>
-  <?php if(is_mobile()) { //スマホの場合 ?>
-  <div style="padding-top:10px;">
-    <?php get_template_part('ad'); //アドセンス読み込み ?>
-  </div>
-  <?php } else { //PCの場合 ?>
-  <?php } ?>
-<?php } ?>
-  <!-- /スマホだけのアドセンス -->
-  <div id="scrollad">
-      <?php get_template_part('scroll-ad'); //追尾式広告 ?>
   </div>
 </aside>
 </div>
