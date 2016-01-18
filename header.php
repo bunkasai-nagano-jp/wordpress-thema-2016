d504c3efe216562114df8e1abdd86657f76783f4
sugiyama <sugiyama@masato.nagano.jp>
2016-01-18 17:28:46 +0900

    [modify]title修正

--- header.php
+++ header.php
@@ -25,27 +25,7 @@
     <?php elseif(is_paged()): ?>
         <meta name="robots" content="noindex,follow">
     <?php endif; ?>
-    <title>
-        <?php
-        global $page, $paged;
-        if(is_front_page()):
-          elseif(is_single()):
-            wp_title('|',true,'right');
-          elseif(is_page()):
-            wp_title('|',true,'right');
-          elseif(is_archive()):
-            wp_title('|',true,'right');
-          elseif(is_search()):
-            wp_title('|',true,'right');
-          elseif(is_404()):
-            echo'404 |';
-          endif;
-          bloginfo('name');
-          if($paged >= 2 || $page >= 2):
-            echo'-'.sprintf('%sページ',
-              max($paged,$page));
-        endif; ?>
-    </title>
+		<title><?php wp_title(); ?></title>
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/normalize.css">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
     <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
