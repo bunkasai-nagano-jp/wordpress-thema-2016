37eaef8e3c24db2fc7e761672505c43dfa4470a1
sugiyama <sugiyama@masato.nagano.jp>
2016-01-18 21:38:25 +0900

    [clean]不要な条件分岐を削除

--- archive.php
+++ archive.php
@@ -10,7 +10,7 @@
       <?php $postcat = get_the_category(); ?>
       <?php $catid = $postcat[0]->cat_ID; ?>
       <?php $allcats = array($catid); ?>
-      <?php 
+      <?php
 while(!$catid==0) {	/* すべてのカテゴリーIDを取得し配列にセットするループ */
     $mycat = get_category($catid); 	/* カテゴリーIDをセット */
     $catid = $mycat->parent; 	/* 上で取得したカテゴリーIDの親カテゴリーをセット */
@@ -24,8 +24,6 @@ $allcats = array_reverse($allcats);
       <div itemscope itemtype="http://data-vocabulary.org/Breadcrumb"> <a href="<?php echo get_category_link($catid); ?>" itemprop="url"> <span itemprop="title"><?php echo get_cat_name($catid); ?></span> </a> &gt; </div>
       <?php endforeach; ?>
     </div>
-    <!--/kuzu-->
-          <!--ループ開始-->
           <h2>
             <?php if( is_category() ) { ?>
             <?php single_cat_title(); ?>
@@ -33,21 +31,11 @@ $allcats = array_reverse($allcats);
             <?php single_tag_title(); ?>
             <?php } elseif( is_tax() ) { ?>
             <?php single_term_title(); ?>
-            <?php } elseif (is_day()) { ?>
-            日別アーカイブ：<?php echo get_the_time('Y年m月d日'); ?>
-            <?php } elseif (is_month()) { ?>
-            月別アーカイブ：<?php echo get_the_time('Y年m月'); ?>
-            <?php } elseif (is_year()) { ?>
-            年別アーカイブ：<?php echo get_the_time('Y年'); ?>
-            <?php } elseif (is_author()) { ?>
-            投稿者アーカイブ：<?php echo esc_html(get_queried_object()->display_name); ?>
             <?php } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
               ブログアーカイブ
               <?php } ?>
-            の文化祭一覧 </h2>
+             の文化祭 </h2>
           <?php get_template_part('itiran');?>
-        <!--/stinger--> 
-        <!--ページナビ-->
         <?php if (function_exists("pagination")) {
 pagination($wp_query->max_num_pages);
 } ?>
