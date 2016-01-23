9f2fa481a6ebdbeafb53cf0b2cf4d3580568d96a
sugiyama <sugiyama@masato.nagano.jp>
2016-01-23 16:02:21 +0900

    [modify]整理

--- home.php
+++ home.php
@@ -1,19 +1,16 @@
 <?php get_header(); ?>
 
 <div id="content" class="clearfix">
-  <div id="contentInner">
-    <main>
-      <article>
-          <?php get_template_part('itiran');?>
-          <!-- ページナビ -->
-          <?php if (function_exists("pagination")) {
-            pagination($wp_query->max_num_pages);
-        } ?>
-    </article>
-</main>
-</div>
-<!-- /#contentInner -->
+    <div id="contentInner">
+        <main>
+            <article>
+                <?php get_template_part('itiran');?>
+                <?php if (function_exists("pagination")) {
+                  pagination($wp_query->max_num_pages);
+              } ?>
+            </article>
+        </main>
+    </div>
 <?php get_sidebar(); ?>
 </div>
-<!-- /#content -->
 <?php get_footer(); ?>
