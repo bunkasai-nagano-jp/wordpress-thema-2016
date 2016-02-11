83f6a572be7d45e0c4b461b7d01997dab1c083a8
sugiyama <sugiyama@masato.nagano.jp>
2016-02-11 21:23:57 +0900

    [modify]整理

--- index.php
+++ index.php
@@ -7,16 +7,12 @@
 				<section>
 					<?php get_template_part('itiran');?>
 				</section>
-				<!--/section-->
-				<!--ページナビ-->
 				<?php if (function_exists("pagination")) {
 					pagination($wp_query->max_num_pages);
 				} ?>
 			</article>
 		</main>
 	</div>
-	<!-- /#contentInner -->
 	<?php get_sidebar(); ?>
 </div>
-<!--/#content -->
 <?php get_footer(); ?>
