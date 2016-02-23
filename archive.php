3208559a4a5c713fb69a840c27c97e45600447ba
sugiyama <sugiyama@masato.nagano.jp>
2016-02-23 19:40:38 +0900

    [modify]インデント調整

--- archive.php
+++ archive.php
@@ -35,8 +35,8 @@
       </h2>
           <?php get_template_part('itiran');?>
         <?php if (function_exists("pagination")) {
-pagination($wp_query->max_num_pages);
-} ?>
+          pagination($wp_query->max_num_pages);
+        } ?>
       </article>
     </main>
   </div>
