2c9daf1f0d3347a27af31781d1472a0f4782dc8f
sugiyama <sugiyama@masato.nagano.jp>
2016-02-26 23:29:24 +0900

    [modify]パンくずの出力方法を変更

--- archive.php
+++ archive.php
@@ -1,38 +1,15 @@
 <?php get_header(); ?>
-
-<div id="content" class="clearfix">
-  <div id="contentInner">
-    <main>
-      <article>
-    <div id="breadcrumb">
-      <div itemscope itemtype="http://data-vocabulary.org/Breadcrumb"> <a href="<?php echo home_url(); ?>" itemprop="url"> <span itemprop="title">ホーム</span> </a> &gt; </div>
-      <?php /*--- カテゴリーが階層化している場合に対応させる --- */ ?>
-      <?php $postcat = get_the_category(); ?>
-      <?php $catid = $postcat[0]->cat_ID; ?>
-      <?php $allcats = array($catid); ?>
-      <?php
-        while(!$catid==0) {	/* すべてのカテゴリーIDを取得し配列にセットするループ */
-          $mycat = get_category($catid); 	/* カテゴリーIDをセット */
-          $catid = $mycat->parent; 	/* 上で取得したカテゴリーIDの親カテゴリーをセット */
-          array_push($allcats, $catid);
-        }
-        array_pop($allcats);
-        $allcats = array_reverse($allcats);
-      ?>
-      <?php /*--- 親カテゴリーがある場合は表示させる --- */ ?>
-      <?php foreach($allcats as $catid): ?>
-      <div itemscope itemtype="http://data-vocabulary.org/Breadcrumb"> <a href="<?php echo get_category_link($catid); ?>" itemprop="url"> <span itemprop="title"><?php echo get_cat_name($catid); ?></span> </a> &gt; </div>
-      <?php endforeach; ?>
-    </div>
-      <h2>
-        <?php if ( is_category() ) {
+<div class="col-md-8">
+  <main>
+    <article>
+      <?php get_template_part('breadcrumb'); ?>
+      <h1 class="title"><?php if ( is_category() ) {
                 single_cat_title();
               } elseif ( is_tag() ) {
                 single_tag_title();
               } elseif ( is_tax() ) {
                 single_term_title();
-              } ?>の文化祭
-      </h2>
+              } ?>の文化祭</h2>
           <?php get_template_part('itiran');?>
         <?php if (function_exists("pagination")) {
           pagination($wp_query->max_num_pages);
@@ -40,7 +17,5 @@
       </article>
     </main>
   </div>
-  <?php get_sidebar(); ?>
-</div>
-
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
