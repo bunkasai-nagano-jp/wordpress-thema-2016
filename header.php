fbf583eadc46fae9c5d32423e9ef6727551bcefa
sugiyama <sugiyama@masato.nagano.jp>
2016-02-26 23:36:50 +0900

    [modify]デザインの変更

--- header.php
+++ header.php
@@ -10,23 +10,32 @@
         <?php wp_head(); ?>
         <link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/images/apple-touch-icon-precomposed.png" />
     </head>
-    <body <?php body_class(); ?>>
-        <div id="wrapper">
-            <header>
-                <p class="sitename">
-                    <a href="<?php echo home_url(); ?>/">
-                    <?php bloginfo( 'name' ); ?>
-                    </a>
-                </p>
-                <nav class="clearfix">
-                    <a class="pc-none" id="toggler" href="#"><i class="fa fa-fw fa-bars"></i>MENU</a>
-                    <?php
-                        $defaults = array(
+    <body class="no-thank-yu" <?php body_class(); ?>>
+        <header>
+          <nav class="navbar navbar-default">
+            <div class="container-fluid">
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="navbar-collapse-1" aria-expanded="false" name="button">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a href="<?php echo home_url(); ?>/" class="navbar-brand"><?php bloginfo( 'name' ); ?></a>
+              </div>
+              <div class="collapse navbar-collapse" id="navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                  <?php
+                    $defaults = array(
                                       'theme_location'  => 'navbar',
-                                      'container' => false ,
-                                      'items_wrap' => '<ul id="menu">%3$s</ul>'
-                                    );
-                        wp_nav_menu( $defaults );
-                    ?>
-                </nav>
-            </header>
+                                      'container' => '' ,
+                                      'items_wrap' => '%3$s'
+                                );
+                    wp_nav_menu( $defaults );
+                  ?>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </header>
+        <div class="container">
