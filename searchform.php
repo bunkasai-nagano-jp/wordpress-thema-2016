8d1eaf7f39358c13cdcd0cfe813b5707ec70eddc
sugiyama <sugiyama@masato.nagano.jp>
2016-02-26 11:19:46 +0900

    [modify]検索フォームの修正
    ドキュメントを参考に修正
    https://wpdocs.osdn.jp/%E9%96%A2%E6%95%B0%E3%83%AA%E3%83%95%E3%82%A1%E3%83%AC%E3%83%B3%E3%82%B9/get_search_form

--- searchform.php
+++ searchform.php
@@ -1,9 +1,11 @@
-<form method="get" action="<?php echo home_url(); ?>/">
+<form role="search" method="get" class="search-form" action="<?php echo home_url(); ?>/">
   <div class="input-group">
-    <label class="hidden" for="s"><?php _e('', 'kubrick'); ?></label>
-    <input type="text" class="form-control" value="<?php the_search_query(); ?>"  name="s"/>
-    <span class="input-group-btn">
-      <button type="submit" class="btn btn-default form-control" alt="検索" value="<?php _e('Search', 'kubrick'); ?>">検索</button>
-    </span>
+    <label for="s">検索</label>
+    <div class="input-group">
+      <input type="search" class="form-control" value="<?php the_search_query(); ?>" id="s" name="s"/>
+      <span class="input-group-btn">
+        <button type="submit" class="btn btn-default form-control" alt="検索" value="<?php _e('Search', 'kubrick'); ?>">検索</button>
+      </span>
+    </div>
   </div>
 </form>
