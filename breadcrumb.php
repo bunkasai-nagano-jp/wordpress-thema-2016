a3cb2e95bdccca8f11c25fa01df927cdd2ce61f6
sugiyama <sugiyama@masato.nagano.jp>
2016-02-27 20:01:47 +0900

    [modify]ホームでの条件分岐を追加

--- breadcrumb.php
+++ breadcrumb.php
@@ -1,13 +1,21 @@
 <ol class="breadcrumb">
-	<li itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="<?php echo home_url(); ?>" itemprop="url"><span itemprop="title">ホーム</span></a></li>
+<?php
+	if ( is_home() )
+	{
+		echo '<li class="active" itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><span itemprop="title">ホーム</span></li>';
+	}
+	else {
+		echo '<li itemscope itemtype="http://data-vocabulary.org/Breadcrumb"><a href="'.home_url().'" itemprop="url"><span itemprop="title">ホーム</span></a></li>';
+	}
+?>
 <?php $postcat = get_the_category();
 $catid = $postcat[0]->cat_ID;
 $allcats = array($catid);
 while( ! $catid == 0 )
 {
-$mycat = get_category($catid);
-$catid = $mycat->parent;
-array_push($allcats, $catid);
+	$mycat = get_category($catid);
+	$catid = $mycat->parent;
+	array_push($allcats, $catid);
 }
 array_pop($allcats);
 $allcats = array_reverse($allcats);
@@ -15,10 +23,14 @@ $tmp = count($allcats);
 ?>
 <?php foreach( $allcats as $catid )
 {
+	if ( is_home() )
+	{
+		break;
+	}
 if( ($tmp == 1) )
 {
-echo '<li class="active" itemscope itemtype="http://data-vocabulary.org/Breadcrumb">'.'<span itemprop="title">'.get_cat_name($catid).'</span></li>';
-break;
+	echo '<li class="active" itemscope itemtype="http://data-vocabulary.org/Breadcrumb">'.'<span itemprop="title">'.get_cat_name($catid).'</span></li>';
+	break;
 }
 echo '<li itemscope itemtype="http://data-vocabulary.org/Breadcrumb">'.'<a href="'.get_category_link($catid).'" itemprop="url">'.'<span itemprop="title">'.get_cat_name($catid).'</span></a></li>';
 $tmp--;
