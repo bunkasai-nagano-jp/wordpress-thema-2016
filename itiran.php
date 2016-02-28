1726bff124c92c2fa1a13f53d19bcbac90d152c6
sugiyama <sugiyama@masato.nagano.jp>
2016-02-28 11:28:13 +0900

    [modify]インデントを揃えた

--- itiran.php
+++ itiran.php
@@ -12,31 +12,37 @@
         <dt>学校名</dt>
         <dd><?php echo get_custom_field('class') ?> <?php echo get_custom_field('schoolName') ?></dd>
 <?php
-  $start_date = get_custom_field('startDate');
-  $end_date = get_custom_field('endDate');
-  if ( ! empty($start_date) )
-  {
-    echo '<dt>開催期間</dt>';
-    echo '<dd>'. $start_date .'&nbsp;~&nbsp;'. $end_date .'</dd>';
-  }
+        $start_date = get_custom_field('startDate');
+        $end_date = get_custom_field('endDate');
+        if ( ! empty($start_date) )
+        {
+?>
+        <?php echo '<dt>開催期間</dt>'."\n"; ?>
+        <?php echo '<dd>'. $start_date .'&nbsp;~&nbsp;'. $end_date .'</dd>'."\n"; ?>
+<?php
+        }
 ?>
 <?php
-  $public_start_date = get_custom_field('publicStartDate');
-  $public_end_date = get_custom_field('publicEndDate');
-  if ( empty($public_start_date) )
-  {
+        $public_start_date = get_custom_field('publicStartDate');
+        $public_end_date = get_custom_field('publicEndDate');
+        if ( empty($public_start_date) )
+        {
 
-  }
-  elseif ( empty($public_end_date) )
-  {
-    echo '<dt>一般公開日</dt>';
-    echo '<dd>'. $public_start_date .'</dd>';
-  }
-  else
-  {
-    echo '<dt>一般公開期間<dt>';
-    echo '<dd>'. $public_start_date .'&nbsp;~&nbsp;'. $public_end_date .'</dd>';
-  }
+        }
+        elseif ( empty($public_end_date) )
+        {
+?>
+        <?php echo '<dt>一般公開日</dt>'."\n"; ?>
+        <?php echo '<dd>'. $public_start_date .'</dd>'."\n"; ?>
+<?php
+        }
+        else
+        {
+?>
+        <?php echo '<dt>一般公開期間<dt>'."\n"; ?>
+        <?php echo '<dd>'. $public_start_date .'&nbsp;~&nbsp;'. $public_end_date .'</dd>'."\n"; ?>
+<?php
+        }
 ?>
       </dl>
       <a class="btn btn-primary pull-right" href="<?php the_permalink(); ?>">詳細を見る</a>
