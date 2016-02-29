9bfa24fb480a01579c5e37bae959b170a78b0dcb
sugiyama <sugiyama@masato.nagano.jp>
2016-02-29 21:24:44 +0900

    [modify]Bootstrapの登録の変更

--- functions.php
+++ functions.php
@@ -31,11 +31,12 @@ function register_open_sans_style()
 }
 add_action('wp_enqueue_scripts', 'register_open_sans_style');
 
-function register_bootstrap_style()
+function register_bootstrap()
 {
-	wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.css', array(), false, false);
+	wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.css', array(), false, false);
+	wp_enqueue_script('bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js', array(), false, false);
 }
-add_action('wp_enqueue_scripts', 'register_bootstrap_style');
+add_action('wp_enqueue_scripts', 'register_bootstrap');
 
 function register_theme_style()
 {
