4a2e7bfb5cfe0de30a8e536b98e550ca8d8c5696
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-06-22 09:10:20 +0900

    [remove] ptengine削除

--- footer.php
+++ footer.php
@@ -5,22 +5,6 @@
     <p>&copy; <?php echo bloginfo('name'); ?> 2015</p>
   </div>
 </footer>
-
-<!-- <script type="text/javascript">
-	  window._pt_lt = new Date().getTime();
-	  window._pt_sp_2 = [];
-	  _pt_sp_2.push('setAccount,7bc15af7');
-	  var _protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-	  (function() {
-		var atag = document.createElement('script'); atag.type = 'text/javascript'; atag.async = true;
-		atag.src = _protocol + 'js.ptengine.jp/pta.js';
-		var stag = document.createElement('script'); stag.type = 'text/javascript'; stag.async = true;
-		stag.src = _protocol + 'js.ptengine.jp/pts.js';
-		var s = document.getElementsByTagName('script')[0];
-		s.parentNode.insertBefore(atag, s);s.parentNode.insertBefore(stag, s);
-	  })();
-</script> -->
-
 <?php wp_footer(); ?>
     </body>
 </html>
