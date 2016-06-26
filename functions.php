118a6cb277fd743f83424ff06dfeebf58c631cb9
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-06-26 11:48:15 +0900

    [modify] functions.php の整理

--- functions.php
+++ functions.php
@@ -1,248 +1,230 @@
 <?php
-function vc_remove_wp_ver_css_js($src)
-{
-if ( strpos($src, 'ver=') )
+add_theme_support('title-tag');
+
+function vc_remove_wp_ver_css_js($src) {
+	if ( strpos($src, 'ver=') )
 	$src = remove_query_arg('ver', $src);
 	return $src;
 }
 add_filter('style_loader_src', 'vc_remove_wp_ver_css_js', 9999);
 add_filter('script_loader_src', 'vc_remove_wp_ver_css_js', 9999);
 
-function register_bootstrap () {
-	wp_enqueue_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css', array(), false, false);
-	wp_enqueue_script('bootstrap-script', 'https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js', array(), false, false);
-}
-
-add_action('wp_enqueue_scripts', "register_bootstrap");
-
-function register_jq_script()
-{
-	if ( ! is_admin() )
-  {
-		$script_dir = get_template_directory_uri();
+function register_jq_script() {
+	if ( !is_admin() ) {
 		wp_deregister_script('jquery');
 		wp_enqueue_script('jquery','https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js', array(), false, false);
 	}
 }
 add_action('wp_enqueue_scripts', 'register_jq_script');
 
-function register_fa_style()
-{
+function register_bootstrap () {
+	wp_enqueue_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css', array(), false, false);
+	wp_enqueue_script('tether-script', 'https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.2/js/tether.min.js', array(), false, false);
+	wp_enqueue_script('bootstrap-script', 'https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js', array(), false, false);
+}
+add_action('wp_enqueue_scripts', "register_bootstrap");
+
+function register_fa_style() {
 	wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css', array(), false, false);
 }
 add_action('wp_enqueue_scripts', 'register_fa_style');
 
-function register_theme_style()
-{
-	wp_enqueue_style('theme_style', get_template_directory_uri() . '/style.css', array(), false, false);
+function register_theme_style() {
+	wp_enqueue_style('theme_style', get_template_directory_uri(). '/style.css', array(), false, false);
 }
 add_action('wp_enqueue_scripts', 'register_theme_style');
 
-//WordPress の投稿スラッグを自動的に生成する
-function auto_post_slug($slug, $post_ID, $post_status, $post_type)
-{
-	if ( preg_match('/(%[0-9a-f]{2})+/', $slug) )
-	{
-		$slug = utf8_uri_encode($post_type) . '-' . $post_ID;
-	}
-	return $slug;
-}
-add_filter('wp_unique_post_slug', 'auto_post_slug', 10, 4);
-
-// 抜粋の長さを変更する
-function custom_excerpt_length($length)
-{
-	return 40;
-}
-add_filter('excerpt_length', 'custom_excerpt_length', 999);
-
-// 文末文字を変更する
-function custom_excerpt_more($more)
-{
-	return ' ... ';
-}
-add_filter('excerpt_more', 'custom_excerpt_more');
-
-//アイキャッチサムネイル
-add_theme_support('post-thumbnails');
-add_image_size('thumb100', 100, 100, true);
-add_image_size('thumb150', 150, 150, true);
-
-
 //カスタムメニュー
-register_nav_menus(array('navbar' => 'ナビゲーションバー'));
-
-//RSS
-add_theme_support('automatic-feed-links');
+register_nav_menus(
+	array(
+	'navbar' => __( 'Navbar', 'wp-thema' ),
+	)
+);
 
 // 管理画面にオリジナルのスタイルを適用
 add_editor_style("style.css");		// メインのCSS
 add_editor_style('editor-style.css');		// これは入れておこう
-if ( ! isset($content_width) ) $content_width = 580;
-function custom_editor_settings($initArray)
-{
-	$initArray['body_id'] = 'primary';		// id の場合はこれ
-	$initArray['body_class'] = 'post';		// class の場合はこれ
-	return $initArray;
+
+if ( !isset($content_width) ) $content_width = 580;
+
+function custom_editor_settings($initArray) {
+		$initArray['body_id'] = 'primary';		// id の場合はこれ
+		$initArray['body_class'] = 'post';		// class の場合はこれ
+		return $initArray;
 }
 add_filter('tiny_mce_before_init', 'custom_editor_settings');
 
-//投稿用ファイルを読み込む
-get_template_part('functions/create-thread');
-
 //ページャー機能
-function pagination ($pages = '', $range = 4)
-{
+function pagination ($pages = '', $range = 4) {
 	$showitems = ($range * 2)+1;
 	global $paged;
-	if ( empty($paged) ) $paged = 1;
-	if ( $pages == '' )
-	{
+	if (!$paged) {
+		$paged = 1;
+	}
+	if ( $pages == '' ) {
 		global $wp_query;
 		$pages = $wp_query->max_num_pages;
-		if ( ! $pages )
-		{
+		if (!$pages) {
 			$pages = 1;
 		}
 	}
-	if ( 1 != $pages )
-	{
-		echo "<div><nav class=\"text-center\"><ul class=\"pagination pagination-lg\">";
-		echo "<li><a href='" . get_pagenum_link($paged - 1) . "' aria-label=\"Previous\"><span aria-hidden=\"true\">&laquo;</span></a></li>";
-		for ( $i=1; $i <= $pages; $i++ )
-		{
-			if ( 1 != $pages &&( ! ($i >= $paged+$range+1 OR $i <= $paged-$range-1) OR $pages <= $showitems) )
-			{
-				echo ($paged == $i)? "<li class=\"active\"><a href=\"#\">".$i."<span class=\"sr-only\">(current)</span></a></li>":"<li><a href='" . get_pagenum_link($i) . "'>" . $i . "</a></li>";
-			}
+	if ( 1 != $pages ) { ?>
+		<div class="row">
+			<div class="col-md-12 text-xs-center">
+				<ul class="pagination">
+					<?php if ($paged == 1) { ?>
+					<li class="page-item disabled"><a class="page-link" href="" aria-label="Previous">
+						<span aria-hidden="true">&laquo;</span>
+						<span class="sr-only">Previous</span></a>
+					</li><?php
+						}
+						else { ?>
+					<li class="page-item"><a class="page-link" href="<?php echo get_pagenum_link($paged - 1); ?>" aria-label="Previous">
+						<span aria-hidden="true">&laquo;</span>
+						<span class="sr-only">Previous</span></a>
+					</li><?php
+						}
+						for ( $i = 1; $i <= $pages; $i++ ) {
+							if ( 1 != $pages &&( ! ($i >= $paged+$range+1 OR $i <= $paged-$range-1) OR $pages <= $showitems) )
+							{
+								if ($paged == $i) {
+									?>
+									<li class="page-item active">
+										<a class="page-link" href="#"><?php echo $i; ?> <span class="sr-only">(current)</span></a>
+									</li>
+									<?php
+								}
+								elseif ($paged != $i) {
+									?>
+									<li class="page-item">
+										<a class="page-link" href="<?php echo get_pagenum_link($i); ?>"><?php echo $i; ?></a>
+									</li>
+									<?php
+								}
+							}
+						}
+						if ($paged == $showitems) { ?>
+							<li class="page-item disabled">
+								<a class="page-link" href="" aria-label="Next">
+									<span aria-hidden="true">&raquo;</span>
+									<span class="sr-only">Next</span>
+								</a>
+							</li>
+							<?php
+						}
+						else { ?>
+							<li class="page-item">
+								<a class="page-link" href="<?php echo get_pagenum_link($paged +1); ?>" aria-label="Next">
+									<span aria-hidden="true">&raquo;</span>
+									<span class="sr-only">Next</span>
+								</a>
+							</li>
+							<?php
+						}
+						?>
+					</ul>
+			</div>
+		</div>
+			<?php
 		}
-		echo "<li><a href=\"" . get_pagenum_link($paged +1) . "\" aria-label=\"Next\"><span aria-hidden=\"true\">&raquo;</span></a></li>";
-		echo "</ul></nav></div>\n";
 	}
-}
 
-//moreリンク
-function custom_content_more_link ($output)
-{
-	$output = preg_replace('/#more-[\d]+/i', '', $output);
-	return $output;
-}
-add_filter('the_content_more_link', 'custom_content_more_link');
-
-//セルフピンバック禁止
-function no_self_pingst (&$links)
-{
-	$home = home_url();
-	foreach ( $links as $l => $link )
-	if ( 0 === strpos($link, $home) )
-	unset($links[$l]);
+	//セルフピンバック禁止
+function no_self_pingst (&$links) {
+		$home = home_url();
+		foreach ( $links as $l => $link )
+		if ( 0 === strpos($link, $home) )
+		unset($links[$l]);
 }
 add_action('pre_ping', 'no_self_pingst');
 
-//ウイジェット追加
-function stinger5_widgets_init()
-{
+	//ウイジェット追加
+function stinger5_widgets_init() {
 	register_sidebar(
 		array(
-			'name'=>__('Primary Sidebar', 'stinger5'),
-			'id'						=> 'sidebar-1',
+			'name'          => __('Primary Sidebar', 'stinger5'),
+			'id'            => 'sidebar-1',
 			'before_widget' => '<ul class="hidden-xs hidden-sm"><li>',
-			'after_widget' => '</li></ul>',
-			'before_title' => '<h4 class="menu_underh1">',
-			'after_title' => '</h4>',
+			'after_widget'  => '</li></ul>',
+			'before_title'  => '<h4 class="menu_underh1">',
+			'after_title'   => '</h4>',
 		)
 	);
 }
 add_action('widgets_init', 'stinger5_widgets_init');
 
 //更新日の追加
-function get_mtime($format)
-{
+function get_mtime($format) {
 	$mtime = get_the_modified_time('Ymd');
 	$ptime = get_the_time('Ymd');
-	if ( $ptime > $mtime )
-	{
+	if ( $ptime > $mtime ) {
 		return get_the_time($format);
 	}
-	elseif ( $ptime === $mtime )
-	{
+	elseif ( $ptime === $mtime ) {
 		return null;
 	}
-	else
-	{
+	else {
 		return get_the_modified_time($format);
 	}
 }
 
-//GoogleMap埋め込み
-function get_gmap_url()
-{
+// GoogleMap埋め込み
+function get_gmap_url() {
 	$gmap = [
 		'school_name' => get_field('schoolName'),
 		'width' => get_field('width'),
 		'height' => get_field('height'),
 	];
 	$google_api_key = 'AIzaSyBfgN4KnKmCL5-Wv3hS-LbQPtsxi_xXdRE';
-	if ( ! empty($gmap) )
-	{
+	if ( ! empty($gmap) ) {
 		$iframe = '<iframe width="' . $gmap['width'] . '" height="' . $gmap['height'] . '" frameborder="0" style="border:0" ' . 'src="https://www.google.com/maps/embed/v1/place?key=' . $google_api_key . '&q=' . urlencode($gmap['school_name']) . '" allowfullscreen></iframe>';
-		return '<div class="gmap">' . $iframe . '</div>';
-		}
-};
+				return '<div class="gmap">' . $iframe . '</div>';
+	}
+}
 add_shortcode ('gmap', 'get_gmap_url');
 
-function get_custom_field($field_name)
-{
+function get_custom_field($field_name) {
 	$data = get_field($field_name);
 	return $data;
 }
-function get_custom_field_wrap($attr)
-{
-	if ( empty($attr[0]) )
-	{
+
+function get_custom_field_wrap($attr) {
+	if ( empty($attr[0]) ) {
 		return '引数を指定してください';
 	}
-	else
-	{
+	else {
 		return get_custom_field($attr[0]);
 	}
 }
 add_shortcode ('get_data' , 'get_custom_field_wrap');
 
-function get_gmap_sv_url()
-{
+function get_gmap_sv_url($width = 400, $height = 300) {
 	$base = 'https://maps.googleapis.com/maps/api/streetview?';
 	$google_api_key = 'AIzaSyBfgN4KnKmCL5-Wv3hS-LbQPtsxi_xXdRE';
-	$width = 400;
-	$height = 300;
+	$width = $width;
+	$height = $height;
 	$location = get_field('streetviewLocation');
 	$fov = get_field('streetviewFov');
 	$pitch = get_field('streetviewPitch');
 	$heading = get_field('heading');
-	if ( empty($location) )
-	{
-		$url = get_template_directory_uri() . "/images/no-image.jpg";
-		return $url;
+	if ( !$location ) {
+		return null;
 	}
-	else
-	{
+	else {
 		$url = $base . 'size=' . $width . 'x' . $height	.'&location=' . $location . '&fov=' . $fov . "&pitch=" . $pitch . '&heading=' . $heading .'&key=' . $google_api_key;
 		return $url;
 	}
 }
 add_shortcode ('getGSV', 'get_gmap_sv_url');
 
-function get_school_page()
-{
+function get_school_page() {
 	get_template_part('school');
 }
 add_shortcode('school', 'get_school_page' );
 
-add_theme_support('title-tag');
 
 function get_school_info( $ID , $param) {
-	if ( !empty($ID) )
-	{
+	if ( !empty($ID) ) {
 		$custom_fields = get_post_custom( $ID );
 		$post = get_post( $ID, ARRAY_A );
 		return $post[$param];
@@ -254,11 +236,11 @@ function get_school_info( $ID , $param) {
 }
 
 function is_other_year_post( $school_name ) {
-	$args = array(
-		 'meta_value' => $school_name,
-	);
-	$result = get_posts( $args );
-	$number = count($result);
+	$args    =  array(
+								'meta_value' => $school_name,
+							);
+	$result  =  get_posts( $args );
+	$number  =  count($result);
 	if ($number == 1) {
 		return false;
 	}
@@ -267,138 +249,37 @@ function is_other_year_post( $school_name ) {
 	}
 }
 
-class MY_WP_Widget_Recent_Posts extends WP_Widget {
+class navbar_link_list extends Walker {
+	public function walk( $elements, $max_depth ) {
+		$list = array ();
 
-	public function __construct() {
-		$widget_ops = array('classname' => 'my_widget_recent_entries', 'description' => __( "BootstrapのためのRecent Posts") );
-		parent::__construct('my-recent-posts', __('最近の投稿 Bootstrap'), $widget_ops);
-		$this->alt_option_name = 'my_widget_recent_entries';
-	}
+		foreach ( $elements as $item )
+		$list[] = "<a class='nav-item nav-link' href='$item->url'>$item->title</a>";
 
-	public function widget( $args, $instance ) {
-		if ( ! isset( $args['widget_id'] ) ) {
-			$args['widget_id'] = $this->id;
-		}
-
-		$title = ( ! empty( $instance['title'] ) ) ? $instance['title'] : __( 'My Recent Posts' );
-
-		$title = apply_filters( 'widget_title', $title, $instance, $this->id_base );
-
-		$number = ( ! empty( $instance['number'] ) ) ? absint( $instance['number'] ) : 5;
-		if ( ! $number )
-			$number = 5;
-
-		$r = new WP_Query( apply_filters( 'widget_posts_args', array(
-			'posts_per_page'      => $number,
-			'no_found_rows'       => true,
-			'post_status'         => 'publish',
-			'ignore_sticky_posts' => true
-		) ) );
-
-		if ($r->have_posts()) :
-		?>
-		<?php echo $args['before_widget']; ?>
-		<?php if ( $title ) {
-			echo $args['before_title'] . $title . $args['after_title'];
-		} ?>
-		<ul class="nav nav-pills nav-stacked">
-		<?php while ( $r->have_posts() ) : $r->the_post(); ?>
-			<li>
-				<a href="<?php the_permalink(); ?>"><?php get_the_title() ? the_title() : the_ID(); ?></a>
-			</li>
-		<?php endwhile; ?>
-		</ul>
-		<?php echo $args['after_widget']; ?>
-		<?php
-		wp_reset_postdata();
-
-		endif;
-	}
-
-	public function update( $new_instance, $old_instance ) {
-		$instance = $old_instance;
-		$instance['title'] = sanitize_text_field( $new_instance['title'] );
-		$instance['number'] = (int) $new_instance['number'];
-		return $instance;
-	}
-
-	public function form( $instance ) {
-		$title     = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
-		$number    = isset( $instance['number'] ) ? absint( $instance['number'] ) : 5;
-?>
-		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" /></p>
-
-		<p><label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of posts to show:' ); ?></label>
-		<input class="tiny-text" id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="number" step="1" min="1" value="<?php echo $number; ?>" size="3" /></p>
-<?php
+		return join($list);
 	}
 }
-add_action('widgets_init', function(){
-     register_widget( 'MY_WP_Widget_Recent_Posts' );
-});
 
-class MY_WP_Widget_Categories extends WP_Widget {
+function relation_post() {
+	$category     =  get_the_category();
+	$category_id  =  $category[0]->cat_ID;
+	$post_id      =  get_the_ID();
+	$start_date   =  get_field('startDate');
+	$year         =  date("Y", strtotime($start_date));
 
-	public function __construct()
-	{
-		$widget_ops = array(
-			'classname' => 'widget_categories_bootstrap',
-			'description' => __( "BootstrapのためのCategories" ),
-		);
-		parent::__construct('categories_bootstrap', __('カテゴリー Bootstrap'), $widget_ops);
-	}
-
-	public function widget( $args, $instance )
-	{
-		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? __( 'Categories' ) : $instance['title'], $instance, $this->id_base );
-
-		echo $args['before_widget'];
-		if ( $title ) {
-			echo $args['before_title'] . $title . $args['after_title'];
-		}
-
-		$cat_args = array(
-			'orderby'      => 'name',
-		);
-?>
-<a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseCategories" aria-expanded="false" aria-controls="collapseCategories">
-表示
-</a>
-	<div class="collapse" id="collapseCategories">
-		<ul class="nav nav-pills nav-stacked nav-thin">
-	<?php
-			$outputs = get_categories($cat_args);
-			foreach ($outputs as $output)
-			{
-	?>
-				<li role="presentation"><a href="<?php echo get_category_link($output->cat_ID); ?>"><?php echo $output->cat_name ?><span class="badge"><?php echo $output->count ?></span></a></li>
-	<?php
-			}
-	?>
-		</ul>
-	</div>
-<?php
-	echo $args['after_widget'];
+	$args = array(
+					'meta_query'      =>  array(
+																	array(
+																		'key'      =>  'startDate',
+																		'value'    =>  array($year.'/01/01', $year.'/12/31'),
+																		'compare'  =>  'BETWEEN',
+																		'type'     =>  'DATE'
+																	)
+																),
+					'cat'             =>  $category_id,
+					'posts_per_page'  =>  2,
+					'post__not_in'    =>  array($post_id),
+	);
+	$posts = query_posts($args);
+	return $posts;
 }
-
-	public function update( $new_instance, $old_instance )
-	{
-		$instance = $old_instance;
-		$instance['title'] = sanitize_text_field( $new_instance['title'] );
-		return $instance;
-	}
-
-	public function form( $instance )
-	{
-		$instance = wp_parse_args( (array) $instance, array( 'title' => '') );
-		$title = sanitize_text_field( $instance['title'] );
-		?>
-		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e( 'Title:' ); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" /></p>
-		<?php
-	}
-}
-add_action('widgets_init', function(){
-     register_widget( 'MY_WP_Widget_Categories' );
-});
