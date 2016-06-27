07a6c6a8a19eff69aed2d18359c7d7e1ee4cb657
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-06-27 15:37:07 +0900

    [modify] コメント追加
    divの閉じタグの位置を変更

--- footer.php
+++ footer.php
@@ -1,10 +1,12 @@
-</div>
-<!-- /container -->
+<!-- footer -->
 <footer class="footer">
   <div class="container">
     <p>&copy; <?php echo bloginfo('name'); ?> 2015</p>
   </div>
 </footer>
+<!-- /footer -->
+</div>
+<!-- /container -->
 <?php wp_footer(); ?>
 <script type="text/javascript" src="//www.google.com/cse/brand?form=cse-search-box&lang=ja"></script>
 </body>
