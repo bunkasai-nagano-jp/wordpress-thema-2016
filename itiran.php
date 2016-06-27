cf7ddd1c8ca61251979a36ab82f0ee10a22f07a5
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-06-27 16:10:40 +0900

    [modify] 改行の修正

--- itiran.php
+++ itiran.php
@@ -1,77 +1,88 @@
-<div class="card-columns">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php
-  $name               =  get_custom_field('name');
-  $class              =  get_custom_field('class');
-  $school_name        =  get_custom_field('schoolName');
-  $start_date         =  get_custom_field('startDate');
-  $end_date           =  get_custom_field('endDate');
-  $public_start_date  =  get_custom_field('publicStartDate');
-  $public_end_date    =  get_custom_field('publicEndDate');
-  $public_unknown     =  get_custom_field('public_unknown');
+<!-- .flex-container -->
+<div class="flex-container">
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+  $name               =  get_field('name');
+  $class              =  get_field('class');
+  $school_name        =  get_field('schoolName');
+  $start_date         =  get_field('startDate');
+  $end_date           =  get_field('endDate');
+  $public_start_date  =  get_field('publicStartDate');
+  $public_end_date    =  get_field('publicEndDate');
+  $public_unknown     =  get_field('public_unknown');
   $today              =  date("Y/m/d");
   $year               =  date("Y", strtotime($start_date));
   $days               =  abs(strtotime($start_date) - strtotime($today)) / (60 * 60 * 24);
-  ?>
-<div class="card">
-  <img class="card-img-top img-fluid" src="<?php echo get_gmap_sv_url(); ?>" alt="streetview">
-  <div class="card-block">
-    <h4 class="card-title"><?php echo $name; ?></h4>
-    <h6 class="card-subtitle text-muted"><?php echo $class; ?> <?php echo $school_name; ?></h6>
-    <h4 class="card-title"><?php
-    if ($year < date("Y")) {
-      echo '終了';
-    }
-    elseif ($days > 0) {
-      echo '開催まで'.$days.'日';
-    }
-    else {
-
-    };
-    ?></h4>
-          <p class="card-text"><?php
-          if ($start_date) {
-            echo <<<EOT
-            <span>開催期間</span>
-            <span>$start_date ~ $end_date</span>
-EOT;
-          }
-          elseif ($public_unknown) {
-            echo <<<EOT
-            <span>一般公開</span>
-            <span>不明</span>
-EOT;
-          }
-          elseif (!$public_start_date) {
-            echo <<<EOT
-            <span>一般公開</span>
-            <span>なし</span>
-EOT;
-          }
-          elseif (!$public_end_date) {
-            echo <<<EOT
-            <span>一般公開日</span>
-            <span>$public_start_date</span>
-EOT;
-          }
-          else {
-            echo <<<EOT
-            <span>一般公開期間<span>
-            <span>{$public_start_date} ~ {$public_end_date}</span>
-EOT;
-          }
-          ?></p>
-          <div class="card-block text-xs-right">
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-        <div class="card-footer">
-          <i class="fa fa-tags"></i> <span><?php the_category(', '); ?></span>
-        </div>
+?>
+  <!-- card -->
+  <div class="card">
+<?php
+  $src = get_gmap_sv_url(385, 200);
+  if ($src) { ?>
+    <a href="<?php the_permalink(); ?>">
+      <img class="card-img-top img-fluid" src="<?php echo $src; ?>" alt="streetview">
+    </a>
+<?php } ?>
+    <div class="card-block">
+      <a href="<?php the_permalink(); ?>">
+        <h2 class="card-title"><?php echo $name; ?></h2>
+      </a>
+      <h6 class="card-subtitle text-muted"><?php echo $class; ?> <?php echo $school_name; ?></h6>
+      <div class="card-block">
+<?php if ($year < date("Y")) { ?>
+        <p class="card-text text-muted"><i class="fa fa-fw fa-clock-o" aria-hidden="true"></i> 終了</p>
+<?php } elseif ($year == date("Y") and strtotime($today) >= strtotime($start_date) and strtotime($today) <= strtotime($end_date)) { ?>
+        <p class="card-text text-info"><i class="fa fa-fw fa-flag" aria-hidden="true"></i> 開催中</p>
+<?php } elseif ($days > 0) { ?>
+        <p class="card-text text-primary"><i class="fa fa-fw fa-clock-o" aria-hidden="true"></i> 開催まで<?php echo $days; ?></p>
+<?php } else { }; ?>
+        <p class="card-text">
+<?php if ($start_date and $end_date) { ?>
+          <span><i class="fa fa-fw fa-calendar"></i> 開催期間</span>
+          <span><?php echo $start_date ?> ~ <?php echo $end_date ?></span>
+<?php } elseif ($start_date and !$end_date) { ?>
+          <span><i class="fa fa-fw fa-calendar"></i> 開催日</span>
+          <span><?php echo $start_date?></span>
+<?php } else { ?>
+          <span><i class="fa fa-fw fa-calendar"></i> 開催期間</span>
+          <span>不明</span>
+<?php } ?>
+        </p>
+        <p class="card-text">
+<?php if ($public_unknown) { ?>
+          <span><i class="fa fa-fw fa-info" aria-hidden="true"></i> 一般公開</span>
+          <span>不明</span>
+<?php } elseif (!$public_start_date and !$public_end_date) { ?>
+          <span><i class="fa fa-fw fa-info" aria-hidden="true"></i> 一般公開</span>
+          <span>なし</span>
+<?php } elseif ($public_start_date and !$public_end_date) { ?>
+          <span><i class="fa fa-fw fa-info" aria-hidden="true"></i> 一般公開日</span>
+          <span><?php echo $public_start_date ?></span>
+<?php } elseif ($public_start_date and $public_end_date) { ?>
+          <span><i class="fa fa-fw fa-info" aria-hidden="true"></i> 一般公開期間<span>
+          <span><?php echo $public_start_date; ?> ~ <?php echo $public_end_date; ?></span>
+<?php } else { ?>
+          <span><i class="fa fa-fw fa-info" aria-hidden="true"></i> 一般公開</span>
+          <span>不明</span>
+<?php } ?>
+        </p>
       </div>
-<?php endwhile; else: ?>
+      <div class="card-block text-xs-right">
+        <a href="<?php the_permalink(); ?>" class="btn btn-primary">詳細</a>
+      </div>
+    </div>
+    <div class="card-footer">
+      <i class="fa fa-tags"></i> <span><?php the_category(', '); ?></span>
+    </div>
   </div>
-<div>
-  <p>記事がありません</p>
-</div>
+  <!-- /card -->
+<?php endwhile; else: ?>
+  <!-- no articles -->
+  <div class="card">
+    <div class="card-block">
+      <p class="card-text">記事がありません</p>
+    </div>
+  </div>
+  <!-- /no articles -->
 <?php endif; ?>
+</div>
+<!-- /.flex-container -->
