5b7394ebc11febfb2ef287f19b264cfd4a6cdbf2
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-06-29 11:37:21 +0900

    [add] コメント追加

--- functions/relation-category-list.php
+++ functions/relation-category-list.php
@@ -1,18 +1,20 @@
 <?php
 
+// 関連する記事を取得する関数
 function relation_category_list ($post_id) {
-  $category     =  get_the_category();
-  $category_id  =  $category[0]->cat_ID;
+  $category     =  get_the_category();   // 現在のカテゴリー(オブジェクト)
+  $category_id  =  $category[0]->cat_ID; // 先頭のカテゴリーID
   $args = array(
-          'meta_query'  =>  array(
-                              'meta'=>array(
+          'meta_query'      =>  array(
+                                  'meta'=>array(
                                         'key'   =>  'startDate',
                                         'type'  =>  'DATE'
-                                      )
-                            ),
-          'cat'         =>  $category_id,
-          'orderby'     =>  'meta',
-          'order'       =>  'DESC',
+                                  )
+                                ),
+          'cat'             =>  $category_id,
+          'orderby'         =>  'meta',
+          'order'           =>  'DESC', // 開催日が新しい順
+          'posts_per_page'  =>  5,      // 最大記事数
           );
   $relation_posts = query_posts($args);
   if (!$relation_posts):
