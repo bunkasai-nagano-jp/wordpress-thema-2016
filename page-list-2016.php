30a2152b8d8cf9da912ae889780d1db32668df96
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-06-29 21:08:41 +0900

    [modify] デザインの変更
    学校名をリンクに
    一般公開を追加

--- page-list-2016.php
+++ page-list-2016.php
@@ -1,77 +1,89 @@
 <?php get_header(); ?>
-<div class="col-md-8">
-  <main>
-    <article>
-      <?php get_template_part('breadcrumb'); ?>
-      <div>
-        <h1 class="title page-header"><?php the_title(); ?></h1>
-        <div class="panel panel-default">
-          <div class="panel-body">
-            <table class="table table-hover table-striped">
-              <thead>
-                <tr>
-                  <th>学校名</th>
-                  <th>文化祭名</th>
-                  <th>開催期間</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php
-                $args = array(
-                          'meta_query' => array(
-                            array(
-                              'key'     =>  'startDate',
-                              'value'   =>  array('2016/01/01', '2016/12/31'),
-                              'compare' =>  'BETWEEN',
-                              'type'    =>  'DATE'
-                            )
-                          ),
-                          'post_type'   => 'post',
-                          'order'       => 'ASC',
-                          'nopaging'    => true,
-                        );
-                $posts = query_posts($args);
-                ?>
-                <?php
-                $tmp = array();
-                foreach ( $posts as $value )
-                {
-                  $school_name = get_post_meta($value->ID, 'schoolName', true);
-                  $start_date  = get_post_meta($value->ID, 'startDate', true);
-                  $end_date    = get_post_meta($value->ID, 'endDate', true);
-                  if ( in_array($school_name, $tmp) )
-                  {
-                    continue;
-                  }
-                  ?>
-                  <tr>
-                    <td><?php echo $school_name ?></td>
-                    <td><?php echo get_post_meta($value->ID, 'name', true); ?></td>
-                    <td><?php
-                    if (!$start_date) {
-                      echo '';
-                    }
-                    elseif (!$end_date) {
-                      echo $start_date;
-                    }
-                    else {
-                      echo $start_date.' ~ '. $end_date;
-                    }
-                    ?></td>
-                  </tr>
-                <?php
-                    $tmp[] = $school_name;
-                }
-                wp_reset_query();
-                ?>
-              </tbody>
-            </table>
-          </div>
-        </div>
+<div class="col-md-12">
+  <div>
+    <div class="card">
+      <div class="card-block">
+        <h1 class="card-title"><?php the_title(); ?></h1>
       </div>
-      <?php get_template_part('sns'); ?>
-    </article>
-  </main>
+      <div class="card-block">
+        <table class="table table-hover table-striped">
+          <thead>
+            <tr>
+              <th>学校名</th>
+              <th>文化祭名</th>
+              <th>開催期間</th>
+              <th>一般公開</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+            $args = array(
+              'meta_query' => array(
+                                'start_date'=>array(
+                                                'key'     =>  'startDate',
+                                                'value'   =>  array('2016/01/01', '2016/12/31'),
+                                                'compare' =>  'BETWEEN',
+                                                'type'    =>  'DATE'
+                                              ),
+                                'address'=>array(
+                                          'key'  =>  'address',
+                                          'type' =>  'CHAR',
+                                        )
+                              ),
+              'post_type'   => 'post',
+              'order'       => 'ASC',
+              'orderby'     => 'address',
+              'nopaging'    => true,
+            );
+            $posts = query_posts($args);
+            ?>
+            <?php
+            $tmp = array();
+            foreach ( $posts as $post ) {
+              $school_name = get_post_meta($post->ID, 'schoolName', true);
+              $start_date  = get_post_meta($post->ID, 'startDate', true);
+              $end_date    = get_post_meta($post->ID, 'endDate', true);
+              $public_start_date  = get_post_meta($post->ID, 'publicStartDate', true);
+              $public_end_date    = get_post_meta($post->ID, 'publicEndDate', true);
+              $public_unknown    = get_post_meta($post->ID, 'public_unknown', true);
+              if ( in_array($school_name, $tmp) ) {
+                continue;
+              } ?>
+              <tr>
+                <td><a href="<?php echo get_permalink($post->ID); ?>"><?php echo $school_name ?></a></td>
+                <td><?php echo get_post_meta($post->ID, 'name', true); ?></td>
+                <td><?php
+                if (!$start_date):
+                  echo '';
+                elseif (!$end_date):
+                  echo $start_date;
+                else:
+                  echo $start_date.' ~ '. $end_date;
+                endif; ?>
+                </td>
+                <td><?php
+                  if ($public_unknown):
+                    echo '不明';
+                  elseif (!$public_start_date and !$public_end_date):
+                    echo 'なし';
+                  elseif ($public_start_date and !$public_end_date):
+                    echo $public_start_date;
+                  elseif ($public_start_date and $public_end_date): ?>
+                    <?php echo $public_start_date; ?> ~ <?php echo $public_end_date; ?>
+                  <?php else:
+                    echo '不明';
+                  endif;
+                ?></td>
+              </tr>
+              <?php
+              $tmp[] = $school_name;
+            }
+            wp_reset_query();
+            ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </div>
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
