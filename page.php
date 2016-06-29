a609e0cb0eac4d34c2d134581b087a0ee21b199a
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-06-29 19:58:57 +0900

    [modify] page.php 修正

--- page.php
+++ page.php
@@ -1,29 +1,37 @@
 <?php get_header(); ?>
-<div class="col-md-8">
+<div class="col-md-12">
   <main>
     <article>
-      <?php get_template_part('breadcrumb'); ?>
       <div>
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <h1 class="title page-header"><?php the_title(); ?></h1>
-        <div class="panel panel-default">
-          <div class="panel-body">
-            <span><i class="fa fa-fw fa-calendar"></i>&nbsp;公開 <time datetime="<?php the_time('c') ;?>"><?php the_time('Y/m/d') ;?></time>&nbsp;<?php if ($mtime = get_mtime('Y/m/d')) echo ' <i class="fa fa-fw fa-repeat"></i>&nbsp;更新 ' , $mtime; ?></span>
-            <hr>
+        <div class="card">
+          <div class="card-block">
+            <h1 class="card-title"><?php the_title(); ?></h1>
+            <h6 class="card-subtitle text-muted">
+              <span><i class="fa fa-fw fa-calendar"></i> 公開 <?php echo get_the_date(); ?></span>
+          <?php if ( get_the_date() != get_the_modified_date() ): ?>
+              <span><i class="fa fa-fw fa-repeat"></i> 更新 <?php echo get_the_modified_date(); ?></span>
+          <?php endif; ?>
+            </h6>
+          </div>
+          <div class="card-block">
             <?php the_content(); ?>
             <?php wp_link_pages(); ?>
           </div>
+          <div class="card-footer">
+            <i class="fa fa-fw fa-tags"></i> <?php the_category(', ') ?> <?php the_tags('', ', '); ?>
+          </div>
         </div>
-      </div>
-      <?php endwhile; else: ?>
-        <div class="panel panel-info">
-          <div class="panel-body">
-            記事がありません
+      <?php endwhile; ?>
+      <?php else: ?>
+        <div class="card">
+          <div class="card-block">
+            <p class="card-text">記事がありません</p>
           </div>
         </div>
       <?php endif; ?>
+      </div>
     </article>
   </main>
 </div>
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
