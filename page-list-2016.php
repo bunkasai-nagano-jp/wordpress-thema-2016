8101c813f5c92889fe7bb96a223d7e58ee01eeec
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-07-01 21:40:23 +0900

    [modify] page-list-2016.php ACF対応
    フィールドの取得方法を変更
    新しい一般公開のフィールド形式に対応

--- page-list-2016.php
+++ page-list-2016.php
@@ -46,29 +46,45 @@
                 <td><a href="<?php echo get_permalink($post->ID); ?>"><?php the_field('schoolName', $post->ID) ?></a></td>
                 <td><?php the_field('name', $post->ID); ?></td>
                 <td><?php
-                if ( !get_field('startDate', $post->ID) ):
-                  echo '';
-                elseif ( get_field('startDate', $post->ID) and !get_field('endDate', $post->ID) ):
-                  the_field('startDate', $post->ID);
-                elseif ( get_field('startDate', $post->ID) and get_field('endDate', $post->ID) ):
-                  echo get_field('startDate', $post->ID).'&nbsp;~&nbsp;'.get_field('endDate', $post->ID);
-                else:
-                  echo '';
-                endif; ?>
+    if ( !get_field('startDate', $post->ID) ):
+      echo '';
+    elseif ( get_field('startDate', $post->ID) and !get_field('endDate', $post->ID) ):
+      the_field('startDate', $post->ID);
+    elseif ( get_field('startDate', $post->ID) and get_field('endDate', $post->ID) ):
+      echo get_field('startDate', $post->ID).'&nbsp;~&nbsp;'.get_field('endDate', $post->ID);
+    else:
+      echo '';
+    endif;
+?>
                 </td>
-                <td><?php
-                  if ($public_unknown):
-                    echo '不明';
-                  elseif ( !get_field('publicStartDate', $post->ID) and !get_field('publicEndDate', $post->ID) ):
-                    echo 'なし';
-                  elseif ( get_field('publicStartDate', $post->ID) and !get_field('publicEndDate', $post->ID) ):
-                    the_field('publicStartDate', $post->ID);
-                  elseif ( get_field('publicStartDate', $post->ID) and get_field('publicEndDate', $post->ID) ):
-                    echo get_field('publicStartDate', $post->ID).'&nbsp;~&nbsp;'.get_field('publicEndDate', $post->ID);
-                  else:
-                    echo '不明';
-                  endif;
-                ?></td>
+                <td>
+<?php
+
+    if ( have_rows('public_open') ):
+
+      while ( have_rows('public_open') ) : the_row();
+        if ( get_sub_field('public_open_day') and get_sub_field('public_open_start_time') and get_sub_field('public_open_end_time') ):
+          echo '<p>'.get_sub_field('public_open_day'). '&nbsp;'. get_sub_field('public_open_start_time'). '&nbsp;~&nbsp;'. get_sub_field('public_open_end_time').'</p>';
+        elseif ( get_sub_field('public_open_day') ):
+          echo '<p>'.get_sub_field('public_open_day').'</p>';
+        else:
+
+        endif;
+      endwhile;
+
+    elseif ( get_field('public_unknown', $post->ID) ):
+      echo '<p>不明</p>';
+    elseif ( get_field('publicStartDate', $post->ID) and get_field('publicEndDate', $post->ID) ):
+      echo '<p>'.get_field('publicStartDate', $post->ID).'&nbsp;~&nbsp;'.get_field('publicEndDate', $post->ID).'</p>';
+    elseif ( get_field('publicStartDate', $post->ID) and !get_field('publicEndDate', $post->ID) ):
+      echo '<p>'.get_field('publicStartDate', $post->ID).'</p>';
+    elseif ( !get_field('publicStartDate', $post->ID) and !get_field('publicEndDate', $post->ID) ):
+      echo '<p>なし</p>';
+    else:
+      echo '<p>不明</p>';
+    endif;
+?>
+                </td>
               </tr>
 <?php
     $tmp[] = get_field('schoolName', $post->ID);
