95050ebe4a69df2b4125369ef35b660137c7f833
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-07-03 15:06:17 +0900

    [modify] archive.php 修正

--- archive.php
+++ archive.php
@@ -1,21 +1,14 @@
 <?php get_header(); ?>
-<div class="col-md-8">
   <main>
-    <article>
-      <?php get_template_part('breadcrumb'); ?>
-      <h1 class="title"><?php if ( is_category() ) {
-                single_cat_title();
-              } elseif ( is_tag() ) {
+    <?php get_template_part('breadcrumb'); ?>
+      <h1><?php if ( is_tag() ) {
                 single_tag_title();
               } elseif ( is_tax() ) {
                 single_term_title();
-              } ?>の文化祭</h2>
+              } ?>の文化祭</h1>
           <?php get_template_part('itiran');?>
-        <?php if (function_exists("pagination")) {
-          pagination($wp_query->max_num_pages);
-        } ?>
-      </article>
     </main>
-  </div>
-<?php get_sidebar(); ?>
+<?php if (function_exists("pagination")) {
+  pagination($wp_query->max_num_pages);
+} ?>
 <?php get_footer(); ?>
