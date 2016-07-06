4e998aa8ae775cdc047f98e90f4c1369247e7264
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-07-06 22:17:42 +0900

    :shirt: コメント追加

--- kanren.php
+++ kanren.php
@@ -3,9 +3,13 @@
     <h2><i class="fa fa-th-list"></i> 関連</h2>
   if ($posts):
 ?>
+<!-- .card-columns -->
 <div class="card-columns">
 <?php foreach($posts as $post): ?>
+<!-- card -->
 <?php get_template_part('template/card'); ?>
+<!-- /card -->
 <?php endforeach; ?>
 </div>
+<!-- /.card-columns -->
 <?php endif; ?>
