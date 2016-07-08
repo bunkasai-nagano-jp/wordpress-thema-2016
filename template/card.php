5408e567327b37ca31aa0cf1b37a5533f1e621af
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-07-08 14:07:21 +0900

    :shirt: コードスタイルの修正

--- template/card.php
+++ template/card.php
@@ -1,13 +1,11 @@
 <div class="card">
   <?php
-  $img_url = get_gmap_sv_url( 385, 200 );
-
-  if ( $img_url ): ?>
-    <a href="<?php the_permalink(); ?>">
-      <img src="<?php echo $img_url ?>" alt="streetview" class="card-img-top img-fluid">
-    </a>
-    <?php
-  endif;
+    $img_url = get_gmap_sv_url( 385, 200 );
+    if ( $img_url ): ?>
+      <a href="<?php the_permalink(); ?>">
+        <img src="<?php echo $img_url ?>" alt="streetview" class="card-img-top img-fluid"> </a>
+      <?php
+    endif;
   ?>
   <div class="card-block">
     <a href="<?php the_permalink(); ?>">
@@ -16,66 +14,62 @@
     <h6 class="card-subtitle text-muted"><?php the_field( 'class' ); ?>&nbsp;<?php the_field( 'schoolName' ); ?></h6>
     <div class="card-block">
       <?php
-      if ( is_bunkasai_during_open() ): ?>
-        <p class="card-text text-info"><i class="fa fa-fw fa-flag" aria-hidden="true"></i>&nbsp;開催中</p>
-        <?php
-      elseif ( is_bunkasai_during_open() === false ): ?>
-        <p class="card-text text-muted"><i class="fa fa-fw fa-clock-o" aria-hidden="true"></i>&nbsp;終了</p>
-        <?php
-      elseif ( is_bunkasai_during_open() === null ): ?>
-        <p class="card-text text-primary">
-          <i class="fa fa-fw fa-clock-o" aria-hidden="true"></i>&nbsp;開催まで<?php echo get_remaining_days(); ?>日</p>
-        <?php
-      endif;
+        if ( is_bunkasai_during_open() ): ?>
+          <p class="card-text text-info"><i class="fa fa-fw fa-flag" aria-hidden="true"></i>&nbsp;開催中</p>
+          <?php
+        elseif ( is_bunkasai_during_open() === false ): ?>
+          <p class="card-text text-muted"><i class="fa fa-fw fa-clock-o" aria-hidden="true"></i>&nbsp;終了</p>
+          <?php
+        elseif ( is_bunkasai_during_open() === null ): ?>
+          <p class="card-text text-primary">
+            <i class="fa fa-fw fa-clock-o" aria-hidden="true"></i>&nbsp;開催まで<?php echo get_remaining_days(); ?>日</p>
+          <?php
+        endif;
       ?>
       <p class="card-text">
         <span><i class="fa fa-fw fa-calendar"></i> 開催期間</span>
         <?php
-        if ( get_field( 'startDate' ) and get_field( 'endDate' ) ): ?>
-          <span><?php the_field( 'startDate' ); ?>&nbsp;~&nbsp;<?php the_field( 'endDate' ); ?></span>
-          <?php
-        elseif ( get_field( 'startDate' ) and !get_field( 'endDate' ) ): ?>
-          <span><?php the_field( 'startDate' ); ?></span>
-        <?php else: ?>
-          <span>不明</span>
-          <?php
-        endif;
+          if ( get_field( 'startDate' ) and get_field( 'endDate' ) ): ?>
+            <span><?php the_field( 'startDate' ); ?>&nbsp;~&nbsp;<?php the_field( 'endDate' ); ?></span>
+            <?php
+          elseif ( get_field( 'startDate' ) and !get_field( 'endDate' ) ): ?>
+            <span><?php the_field( 'startDate' ); ?></span>
+          <?php else: ?>
+            <span>不明</span>
+            <?php
+          endif;
         ?>
       </p>
       <p class="card-text"><i class="fa fa-fw fa-info" aria-hidden="true"></i> 一般公開</p>
       <?php
-      if ( get_field( 'public_unknown' ) ): ?>
-        <p class="card-text">不明</p>
-        <?php
-      else:
-
-        if ( have_rows( 'public_open' ) ):
-
-          while ( have_rows( 'public_open' ) ) : the_row();
-            if ( get_sub_field( 'public_open_day' ) and get_sub_field( 'public_open_start_time' ) and get_sub_field( 'public_open_end_time' ) ):
-              echo '<p class="card-text">';
-              echo get_sub_field( 'public_open_day' ) . '&nbsp;' . get_sub_field( 'public_open_start_time' ) . '&nbsp;~&nbsp;' . get_sub_field( 'public_open_end_time' );
-              echo '</p>';
-            elseif ( get_sub_field( 'public_open_day' ) ): ?>
-              <p class="card-text"><?php the_sub_field( 'public_open_day' ); ?></p>
-              <?php
-            endif;
-          endwhile;
-
-        elseif ( get_field( 'publicStartDate' ) and get_field( 'publicEndDate' ) ):
-          echo '<p class="card-text">';
-          echo get_field( 'publicStartDate' ) . '&nbsp;~&nbsp;' . get_field( 'publicEndDate' );
-          echo '</p>';
-        elseif ( get_field( 'publicStartDate' ) and !get_field( 'publicEndDate' ) ):
-          echo '<p class="card-text">';
-          echo get_field( 'publicStartDate' );
-          echo '</p>';
-        elseif ( !have_rows( 'public_open' ) and !get_field( 'publicStartDate' ) and !get_field( 'publicEndDate' ) ): ?>
-          <p class="card-text">なし</p>
+        if ( get_field( 'public_unknown' ) ): ?>
+          <p class="card-text">不明</p>
           <?php
+        else:
+          if ( have_rows( 'public_open' ) ):
+            while ( have_rows( 'public_open' ) ) : the_row();
+              if ( get_sub_field( 'public_open_day' ) and get_sub_field( 'public_open_start_time' ) and get_sub_field( 'public_open_end_time' ) ):
+                echo '<p class="card-text">';
+                echo get_sub_field( 'public_open_day' ) . '&nbsp;' . get_sub_field( 'public_open_start_time' ) . '&nbsp;~&nbsp;' . get_sub_field( 'public_open_end_time' );
+                echo '</p>';
+              elseif ( get_sub_field( 'public_open_day' ) ): ?>
+                <p class="card-text"><?php the_sub_field( 'public_open_day' ); ?></p>
+                <?php
+              endif;
+            endwhile;
+          elseif ( get_field( 'publicStartDate' ) and get_field( 'publicEndDate' ) ):
+            echo '<p class="card-text">';
+            echo get_field( 'publicStartDate' ) . '&nbsp;~&nbsp;' . get_field( 'publicEndDate' );
+            echo '</p>';
+          elseif ( get_field( 'publicStartDate' ) and !get_field( 'publicEndDate' ) ):
+            echo '<p class="card-text">';
+            echo get_field( 'publicStartDate' );
+            echo '</p>';
+          elseif ( !have_rows( 'public_open' ) and !get_field( 'publicStartDate' ) and !get_field( 'publicEndDate' ) ): ?>
+            <p class="card-text">なし</p>
+            <?php
+          endif;
         endif;
-
-      endif;
       ?>
     </div>
     <div class="card-block text-xs-right">
