36611a496ef928fa465c0bbc02bbb2b548fa1af0
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-07-08 22:50:14 +0900

    :+1: 見出しを追加

--- template/twitter_embed.php
+++ template/twitter_embed.php
@@ -1,5 +1,7 @@
 <?php
-  if ( have_rows( 'twitter_user_timeline' ) ):
+  if ( have_rows( 'twitter_user_timeline' ) or have_rows( 'twitter_search_timeline' ) ): ?>
+    <h2>Twitter</h2>
+    <?php
     while ( have_rows( 'twitter_user_timeline' ) ) : the_row();
       $twitter_user_timeline_screen_name = get_sub_field( 'twitter_user_timeline_screen_name' );
       $twitter_user_timeline_width       = get_sub_field( 'twitter_user_timeline_width' );
@@ -8,8 +10,6 @@
       <a class="twitter-timeline" data-width="<?php echo $twitter_user_timeline_width; ?>" data-height="<?php echo $twitter_user_timeline_height; ?>" href="https://twitter.com/<?php echo $twitter_user_timeline_screen_name; ?>"> Tweets by <?php echo $twitter_user_timeline_screen_name; ?></a>
       <?php
     endwhile;
-    endif;
-  if (have_rows('twitter_search_timeline')):
     while ( have_rows( 'twitter_search_timeline' ) ) : the_row();
       $twitter_search_timeline_name      = get_sub_field( 'twitter_search_timeline_name' );
       $twitter_search_timeline_url       = get_sub_field( 'twitter_search_timeline_url' );
