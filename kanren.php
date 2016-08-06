2128978ccaae38c495e4f2e8c98618bae691ffba
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-08-06 21:31:57 +0900

    :shirt: 文言の変更

--- kanren.php
+++ kanren.php
@@ -1,7 +1,7 @@
 <?php
 $posts = relation_post();
 if ( $posts ): ?>
-  <h2><i class="fa fa-th-list"></i>&nbsp;関連</h2>
+  <h2><i class="fa fa-th-list"></i>&nbsp;関連記事</h2>
   <!-- .card-columns -->
   <div class="card-columns">
     <?php foreach ( $posts as $post ): ?>
