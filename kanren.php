5edc37c55a1ec8d1668080e4361a95aff066b8bb
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-08-08 22:14:02 +0900

    :sparkles: #7 .card-columnsからflex-boxへ作り直す
    flex-boxで作ったほうが少ないCSSでできそう
    .card-columnsに関連する箇所を削除

--- kanren.php
+++ kanren.php
@@ -3,7 +3,7 @@ $posts = relation_post();
 if ( $posts ): ?>
   <h2><i class="fa fa-th-list"></i>&nbsp;関連記事</h2>
   <!-- .card-columns -->
-  <div class="card-columns">
+  <div class="kanren-flex-container">
     <?php foreach ( $posts as $post ): ?>
       <!-- card -->
       <?php get_template_part( 'template/card' ); ?>
