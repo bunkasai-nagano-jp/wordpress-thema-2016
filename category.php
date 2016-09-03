64aa79e8a1fee4d94e5cf705804fe4d9c61760ed
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-09-03 17:15:06 +0900

    :shirt: Fix code style

--- category.php
+++ category.php
@@ -1,39 +1,39 @@
 <?php get_header(); ?>
 <main>
-  <?php get_template_part( 'breadcrumb' ); ?>
-  <h1><?php single_cat_title(); ?>の文化祭</h1>
-  <!-- .flex-container -->
-  <div class="flex-container">
-    <?php
-    $category    = get_the_category();
-    $category_id = $category[0]->cat_ID;
-    $paged       = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-    $args        = array(
-      'meta_query'     => array(
-        'meta' => array(
-          'key'  => 'startDate',
-          'type' => 'DATE'
-        )
-      ),
-      'cat'            => $category_id,
-      'orderby'        => 'meta',
-      'order'          => 'DESC',
-      'posts_per_page' => 10,
-      'paged'          => $paged
-    );
-    $wp_query    = new WP_Query( $args );
-    if ( $wp_query->have_posts() ) : while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
-      <!-- card -->
-      <?php get_template_part( 'template/card' ); ?>
-      <!-- /card -->
-    <?php endwhile; ?>
-    <?php else: ?>
-      <!-- no articles -->
-      <?php get_template_part( 'template/no_articles' ); ?>
-      <!-- /no articles -->
-    <?php endif; ?>
-  </div>
-  <!-- /.flex-container -->
+	<?php get_template_part( 'breadcrumb' ); ?>
+	<h1><?php single_cat_title(); ?>の文化祭</h1>
+	<!-- .flex-container -->
+	<div class="flex-container">
+		<?php
+			$category    = get_the_category();
+			$category_id = $category[0]->cat_ID;
+			$paged       = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+			$args        = [
+				'meta_query'     => [
+					'meta' => [
+						'key'  => 'startDate',
+						'type' => 'DATE',
+					],
+				],
+				'cat'            => $category_id,
+				'orderby'        => 'meta',
+				'order'          => 'DESC',
+				'posts_per_page' => 10,
+				'paged'          => $paged,
+			];
+			$wp_query    = new WP_Query( $args );
+			if ( $wp_query->have_posts() ) : while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
+				<!-- card -->
+				<?php get_template_part( 'template/card' ); ?>
+				<!-- /card -->
+			<?php endwhile; ?>
+			<?php else : ?>
+				<!-- no articles -->
+				<?php get_template_part( 'template/no_articles' ); ?>
+				<!-- /no articles -->
+			<?php endif; ?>
+	</div>
+	<!-- /.flex-container -->
 </main>
 <?php get_template_part( 'template/pagination' ); ?>
 <?php get_footer(); ?>
