f0bdf5049cb2330c67e068b1955239576a1595e6
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-09-05 18:22:07 +0900

    :shirt: Use Yoda Condition checks

--- template/pagination.php
+++ template/pagination.php
@@ -1,6 +1,6 @@
 <?php
 if ( function_exists( 'pagination' ) ) {
-	if ( $wp_query->max_num_pages !== 1 ) {
+	if ( 1 !== $wp_query->max_num_pages ) {
 		echo "<!-- pagination -->\n";
 		pagination( $wp_query->max_num_pages );
 		echo "<!-- /pagination -->\n";
