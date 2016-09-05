3fbf95b4c917b2bd2134bb58201fa29787cebd1a
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-09-05 15:06:48 +0900

    :shirt: Fix Code Style

--- functions/functions.php
+++ functions/functions.php
@@ -11,32 +11,35 @@ function is_other_year_post( $school_name ) {
 	$number = $result->found_posts;
 	if ( 1 === $number ) :
 		return false;
-	else:
+	else :
 		return true;
 	endif;
 }
 
-// 文化祭が終了しているか
-function is_ended () {
-
-	if ( !get_field( 'endDate' ) ) {
+/**
+ * 文化祭が終了しているか
+ */
+function is_ended() {
+	if ( ! get_field( 'endDate' ) ) {
 		$end_date = get_field( 'startDate' );
 	} else {
 		$end_date = get_field( 'endDate' );
 	}
-	if ( $end_date < date( "Y/m/d" ) ):
+	if ( $end_date < date( 'Y/m/d' ) ) :
 		return true;
-	else:
+	else :
 		return false;
 	endif;
 }
 
-// 文化祭までの残り日数を取得
-function get_remaining_days () {
+/**
+ * 文化祭までの残り日数を取得
+ */
+function get_remaining_days() {
 	if ( get_field( 'startDate' ) and get_field( 'endDate' ) ) {
 		$start_date = get_field( 'startDate' );
 		$end_date   = get_field( 'endDate' );
-		$today      = date( "Y/m/d" );
+		$today      = date( 'Y/m/d' );
 		$days       = abs( strtotime( $start_date ) - strtotime( $today ) ) / ( 60 * 60 * 24 );
 
 		return $days;
@@ -45,9 +48,11 @@ function get_remaining_days () {
 	}
 }
 
-// 終了日を取得
-function get_end_date () {
-	if ( !get_field( 'endDate' ) ) {
+/**
+ * 終了日を取得
+ */
+function get_end_date() {
+	if ( ! get_field( 'endDate' ) ) {
 		$end_date = get_field( 'startDate' );
 	} else {
 		$end_date = get_field( 'endDate' );
@@ -67,26 +72,26 @@ function is_bunkasai_during_open() {
 		$start_date = get_field( 'startDate' );
 		$today      = date( 'Y/m/d' );
 		if ( ! get_field( 'endDate' ) ) {
-			// 開催期間が1日の文化祭を想定する
+			// 開催期間が1日の文化祭を想定する.
 			$end_date = get_field( 'startDate' );
 		} else {
 			$end_date = get_field( 'endDate' );
 		}
 		if ( $today === $start_date ) {
-			// 開始日当日
+			// 開始日当日.
 			return true;
 		} elseif ( $today === $end_date ) {
-			// 終了日当日
+			// 終了日当日.
 			return true;
 		} elseif ( $today > $end_date ) {
-			// 終了日よりも後
+			// 終了日よりも後.
 			return false;
 		} elseif ( $today > $start_date and $today < $end_date ) {
-			// 開催期間中
-			// 開始日よりも後 終了日よりも前
+			// 開催期間中.
+			// 開始日よりも後 終了日よりも前.
 			return true;
 		} elseif ( $today < $start_date ) {
-			// 開始日よりも前 残り日数を計算する
+			// 開始日よりも前 残り日数を計算する.
 			return null;
 		}
 	}
