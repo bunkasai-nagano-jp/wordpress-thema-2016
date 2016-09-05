1175ae55600c46a6ae902febd440a5c434d8c008
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-09-05 19:22:32 +0900

    :shirt: Rename template/twitter_embed.php -> template/twitter-embed.php

--- school.php
+++ school.php
@@ -68,7 +68,7 @@ if ( get_gmap_sv_url( 600, 350 ) ) :  ?>
 		echo '</div>';
 	endif;
 	?>
-	<?php get_template_part( 'template/twitter_embed' ); ?>
+	<?php get_template_part( 'template/twitter-embed' ); ?>
 	<h2 class="card-title">地図</h2>
 	<?php echo get_gmap_url(); ?>
 	<h3 class="card-title">学校情報</h3>
