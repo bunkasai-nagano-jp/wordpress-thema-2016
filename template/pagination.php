e79b1f7d1f263f3d19680710a18b553052304a49
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-09-05 18:19:50 +0900

    :shirt: Use strict comparisons

--- template/pagination.php
+++ template/pagination.php
@@ -1,6 +1,6 @@
 <?php
 if ( function_exists( 'pagination' ) ) {
-	if ( $wp_query->max_num_pages != 1 ) {
+	if ( $wp_query->max_num_pages !== 1 ) {
 		echo "<!-- pagination -->\n";
 		pagination( $wp_query->max_num_pages );
 		echo "<!-- /pagination -->\n";
