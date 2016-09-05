5bae720eaf5cf28303de093fc7fc0a1891a2c39c
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-09-05 12:36:47 +0900

    :shirt: Fix Code Style

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,19 @@
 <?php
-// titleタグのサポート
+/**
+ * TITLEタグのサポート
+ */
 add_theme_support( 'title-tag' );
 
-// head内のlink要素を停止
+/**
+ * HEAD要素内のlink要素を停止
+ */
 remove_action( 'wp-head', 'rsd_link' );
 remove_action( 'wp-head', 'feed_links', 2 );
 remove_action( 'wp-head', 'rsd_links_extra', 3 );
 
-// カスタムメニューの登録
+/**
+ * カスタムメニューの登録
+ */
 register_nav_menus(
 	array(
 		'navbar' => __( 'Navbar', 'wp-thema' ),
@@ -15,7 +21,9 @@ register_nav_menus(
 	)
 );
 
-// セルフピンバック禁止
+/**
+ * セルフピンバック禁止
+ */
 function no_self_pingst( &$links ) {
 	$home = home_url();
 	foreach ( $links as $l => $link ) {
@@ -27,7 +35,9 @@ function no_self_pingst( &$links ) {
 
 add_action( 'pre_ping', 'no_self_pingst' );
 
-// ウィジェット追加
+/**
+ * ウィジェット追加
+ */
 function my_widgets_init() {
 	register_sidebar(
 		array(
@@ -43,7 +53,9 @@ function my_widgets_init() {
 
 add_action( 'widgets_init', 'my_widgets_init' );
 
-// cssとjsのバージョンを削除する
+/**
+ * CSSとJSのバージョンを削除する
+ */
 function vc_remove_wp_ver_css_js( $src ) {
 	if ( strpos( $src, 'ver=' ) ) {
 		$src = remove_query_arg( 'ver', $src );
@@ -55,15 +67,21 @@ function vc_remove_wp_ver_css_js( $src ) {
 add_filter( 'style_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
 add_filter( 'script_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
 
-// 画像サイズを定義
+/**
+ * 画像サイズを定義
+ */
 if ( ! isset( $content_width ) ) {
 	$content_width = 580;
 }
 
-// 管理画面にオリジナルのスタイルを適用
-add_editor_style( 'style.css' ); // メインのCSS
+/**
+ * 管理画面にオリジナルのスタイルを適用
+ */
+add_editor_style( 'style.css' ); // メインのCSS.
 
-// <script>タグにdefer属性を付与する
+/**
+ * <script>タグにdefer属性を付与する
+ */
 function add_async_to_script( $tag ) {
 
 	return str_replace( "type='text/javascript'", 'defer', $tag );
