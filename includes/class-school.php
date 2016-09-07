16fb08a03aae7f88b5e25087996a5a7bddf3ff2f
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-09-07 17:02:55 +0900

    :sparkles: #9 Add function get_public_open()

--- includes/class-school.php
+++ includes/class-school.php
@@ -34,4 +34,46 @@ class School {
 			$start_date = new DateTime( get_field( 'startDate', $id ) );
 		return $start_date->format( 'Y' );
 	}
+
+	/**
+	 * 一般公開期間を取得する
+	 *
+	 * @return array|bool
+	 */
+	private function get_public_open() {
+		$public_open     = [];
+		$public_open_tmp = [];
+		if ( have_rows( 'public_open' ) ) :
+			while ( have_rows( 'public_open' ) ) : the_row();
+				if ( get_sub_field( 'public_open_day' ) and get_sub_field( 'public_open_start_time' ) and get_sub_field( 'public_open_end_time' ) ) :
+					$public_open_start = get_sub_field( 'public_open_day' ) . ' ' . get_sub_field( 'public_open_start_time' );
+					$public_open_end   = get_sub_field( 'public_open_day' ) . ' ' . get_sub_field( 'public_open_end_time' );
+					$public_open[]     = [
+						'public_open_start' => new DateTime( $public_open_start ),
+						'public_open_end'   => new DateTime( $public_open_end ),
+					];
+				elseif ( get_sub_field( 'public_open_day' ) ) :
+						$public_open_tmp[] = new DateTime( get_sub_field( 'public_open_day' ) );
+				endif;
+			endwhile;
+		elseif ( get_field( 'publicStartDate' ) and get_field( 'publicEndDate' ) ) :
+				$public_open_start = get_field( 'publicStartDate' );
+				$public_open_end   = get_field( 'publicEndDate' );
+				$public_open[] = [
+					'public_open_start' => new DateTime( $public_open_start ),
+					'public_open_end'   => new DateTime( $public_open_end ),
+				];
+		endif;
+		if ( $public_open_tmp ) {
+				asort( $public_open_tmp );
+				$public_open['public_open_start'] = array_shift( $public_open_tmp );
+				$public_open['public_open_end']   = array_pop( $public_open_tmp );
+				$public_open_tmp                  = [];
+		}
+		if ( $public_open ) {
+			return $public_open;
+		} else {
+			return false;
+		}
+	}
 }
