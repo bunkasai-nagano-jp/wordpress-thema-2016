583c710626f6fb869da0809d7a4cabb37efebddf
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-09-14 20:26:35 +0900

    :shirt: Fix Code Style

--- school.php
+++ school.php
@@ -1,11 +1,10 @@
 <?php
-
-if ( get_gmap_sv_url( 600, 350 ) ) :  ?>
-	<img class="card-img-top img-fluid" src="<?php echo esc_url( get_gmap_sv_url( 600, 350 ) ); ?>" alt="streetview">
-<?php endif; ?>
+	if ( get_gmap_sv_url( 600, 350 ) ) : ?>
+		<img class="card-img-top img-fluid" src="<?php echo esc_url( get_gmap_sv_url( 600, 350 ) ); ?>" alt="streetview">
+	<?php endif; ?>
 <div class="card-block">
-	<?php if ( is_ended() ) :  ?>
-	<p class="card-text text-muted">この文化祭は終了しました</p>
+	<?php if ( is_ended() ) : ?>
+		<p class="card-text text-muted">この文化祭は終了しました</p>
 	<?php endif; ?>
 	<h2 class="card-title">名称</h2>
 	<p><?php the_field( 'name' ); ?></p>
@@ -19,77 +18,58 @@ if ( get_gmap_sv_url( 600, 350 ) ) :  ?>
 	?>
 	</p>
 	<h2 class="card-title">一般公開</h2>
-	<?php
-	if ( get_field( 'public_unknown' ) ) :
-		echo '<p>';
-		echo '不明';
-		echo '</p>';
-	else :
-		if ( have_rows( 'public_open' ) ) :
+	<?php if ( get_field( 'public_unknown' ) ) : ?>
+		<p>不明</p>
+	<?php else : ?>
+		<?php if ( have_rows( 'public_open' ) ) :
 			while ( have_rows( 'public_open' ) ) : the_row();
-				if ( get_sub_field( 'public_open_day' ) and get_sub_field( 'public_open_start_time' ) and get_sub_field( 'public_open_end_time' ) ) :
-					echo '<p>';
-					echo get_sub_field( 'public_open_day' ) . '&nbsp;' . get_sub_field( 'public_open_start_time' ) . '&nbsp;~&nbsp;' . get_sub_field( 'public_open_end_time' );
-					echo '</p>';
-				  elseif ( get_sub_field( 'public_open_day' ) ) :
-						echo '<p>';
-						the_sub_field( 'public_open_day' );
-						echo '</p>';
-				  endif;
-			  endwhile;
-
-	elseif ( get_field( 'publicStartDate' ) and get_field( 'publicEndDate' ) ) :
-		echo '<p>';
-		echo get_field( 'publicStartDate' ) . '&nbsp;~&nbsp;' . get_field( 'publicEndDate' );
-		echo '</p>';
-	elseif ( get_field( 'publicStartDate' ) ) :
-		echo '<p>';
-		the_field( 'publicStartDate' );
-		echo '</p>';
-	else :
-		echo '<p>';
-		echo 'なし';
-		echo '</p>';
-	endif;
-
-	endif;
-	?>
-	<?php
-	if ( have_rows( 'bunkasai_site' ) ) :
-		echo '<h2 class="card-title">関連サイト</h2>';
-		echo '<div class="card-block">';
-		while ( have_rows( 'bunkasai_site' ) ) :the_row();
-			if ( get_sub_field( 'bunkasai_site_name' ) and get_sub_field( 'bunkasai_site_url' ) ) :
-				$element = '<a href="' . get_sub_field( 'bunkasai_site_url' ) . '">' . get_sub_field( 'bunkasai_site_url' ) . '</a>';
-				echo '<h3>' . get_sub_field( 'bunkasai_site_name' ) . '</h3>';
-				echo $element;
-			endif;
-		endwhile;
-		echo '</div>';
-	endif;
-	?>
+				if ( get_sub_field( 'public_open_day' ) and get_sub_field( 'public_open_start_time' ) and get_sub_field( 'public_open_end_time' ) ) : ?>
+					<p><?php the_sub_field( 'public_open_day' ); ?>&nbsp;<?php the_sub_field( 'public_open_start_time' ); ?>&nbsp;~&nbsp;<?php the_sub_field( 'public_open_end_time' ); ?></p>
+				<?php elseif ( get_sub_field( 'public_open_day' ) ) : ?>
+					<p><?php the_sub_field( 'public_open_day' ); ?></p>
+				<?php endif;
+			endwhile;
+		elseif ( get_field( 'publicStartDate' ) and get_field( 'publicEndDate' ) ) : ?>
+			<p><?php the_field( 'publicStartDate' ); ?>&nbsp;~&nbsp;<?php the_field( 'publicEndDate' ); ?></p>
+		<?php elseif ( get_field( 'publicStartDate' ) ) : ?>
+			<p><?php the_field( 'publicStartDate' ); ?></p>
+		<?php else : ?>
+			<p>なし</p>
+		<?php endif;
+	endif; ?>
+	<?php if ( have_rows( 'bunkasai_site' ) ) : ?>
+		<h2 class="card-title">関連サイト</h2>
+		<div class="card-block">
+			<?php while ( have_rows( 'bunkasai_site' ) ) :the_row(); ?>
+				<?php if ( get_sub_field( 'bunkasai_site_name' ) and get_sub_field( 'bunkasai_site_url' ) ) : ?>
+					<h3><?php the_sub_field( 'bunkasai_site_name' ); ?></h3>
+					<a href="<?php the_sub_field( 'bunkasai_site_url' ) ?>"><?php the_sub_field( 'bunkasai_site_url' ); ?></a>
+				<?php endif;
+			endwhile; ?>
+		</div>
+	<?php endif; ?>
 	<?php get_template_part( 'template/twitter-embed' ); ?>
 	<h2 class="card-title">地図</h2>
 	<?php echo get_gmap_url(); ?>
 	<h3 class="card-title">学校情報</h3>
 	<table class="table table-striped table-bordered">
-	<tbody>
-	<tr>
-	  <td>学校名</td>
-	  <td><?php the_field( 'schoolName' ); ?></td>
-	</tr>
-	<tr>
-	  <td>住所</td>
-	  <td><?php the_field( 'address' ); ?></td>
-	</tr>
-	<tr>
-	  <td>学校公式サイト</td>
-	  <td><a href="<?php the_field( 'url' ); ?>"><?php the_field( 'url' ); ?></a></td>
-	</tr>
-	<tr>
-	  <td>電話番号</td>
-	  <td><?php the_field( 'tel' ); ?></td>
-	</tr>
-	</tbody>
+		<tbody>
+		<tr>
+			<td>学校名</td>
+			<td><?php the_field( 'schoolName' ); ?></td>
+		</tr>
+		<tr>
+			<td>住所</td>
+			<td><?php the_field( 'address' ); ?></td>
+		</tr>
+		<tr>
+			<td>学校公式サイト</td>
+			<td><a href="<?php the_field( 'url' ); ?>"><?php the_field( 'url' ); ?></a></td>
+		</tr>
+		<tr>
+			<td>電話番号</td>
+			<td><?php the_field( 'tel' ); ?></td>
+		</tr>
+		</tbody>
 	</table>
 </div>
