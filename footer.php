fb30335ac334ce0def8b025ce34cfd953e79bcd4
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-09-14 17:56:16 +0900

    :shirt: Fix Code Style

--- footer.php
+++ footer.php
@@ -1,24 +1,24 @@
 <!-- footer -->
 <footer class="footer">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-4">
-        <p>&copy; <?php echo bloginfo( 'name' ); ?> 2015</p>
-	  </div>
-	  <div class="col-md-8">
-		<?php
-		$defaults = array(
-		  'menu'           => 'footer',
-		  'container'      => false,
-		  'echo'           => true,
-		  'depth'          => 0,
-		  'walker'         => new NavbarLinkList,
-		  'theme_location' => 'footer',
-		  'items_wrap'     => '%3$s',
-		);
-		wp_nav_menu( $defaults ); ?>
-	  </div>
-	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-4">
+				<p>&copy;&nbsp;<?php bloginfo( 'name' ); ?>&nbsp;2015</p>
+			</div>
+			<div class="col-md-8">
+				<?php
+					$defaults = [
+						'menu'           => 'footer',
+						'container'      => false,
+						'echo'           => true,
+						'depth'          => 0,
+						'walker'         => new NavbarLinkList,
+						'theme_location' => 'footer',
+						'items_wrap'     => '%3$s',
+					];
+					wp_nav_menu( $defaults ); ?>
+			</div>
+		</div>
 	</div>
 </footer>
 <!-- /footer -->
