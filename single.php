014f457dff7db007810fc32affcdefa9bd0d7668
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-09-15 23:10:56 +0900

    :+1: Improve single.php
    公開日、更新日を出力する関数を変更。

--- single.php
+++ single.php
@@ -8,9 +8,9 @@
 			<div class="card-block">
 			  <h1 class="card-title"><?php the_title(); ?></h1>
 			  <h6 class="card-subtitle text-muted">
-				<p class="card-date"><i class="fa fa-fw fa-calendar"></i> 公開 <?php echo get_the_date(); ?></p>
+				<p class="card-date"><i class="fa fa-fw fa-calendar"></i> 公開 <?php the_date(); ?></p>
 				<?php if ( get_the_date() !== get_the_modified_date() ) :  ?>
-				  <p class="card-date"><i class="fa fa-fw fa-repeat"></i> 更新 <?php echo get_the_modified_date(); ?></p>
+				  <p class="card-date"><i class="fa fa-fw fa-repeat"></i> 更新 <?php the_modified_date(); ?></p>
 				<?php endif; ?>
 			  </h6>
 			</div>
