853d6fc50e09a0ab64dffd1227dd8a476084ac89
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-09-23 17:07:35 +0900

    :sparkles: Add function the_gmap()
    GoogleMapを表示する関数を追加した。

--- functions/google-map.php
+++ functions/google-map.php
@@ -15,7 +15,21 @@ function get_gmap_url() {
 		return false;
 	}
 }
+
+/**
+ * GoogleMapを表示する関数.
+ **/
+function the_gmap() {
+	if ( get_gmap_url() ) {
+		$url         = get_gmap_url();
+		$width_attr  = 'width="' . esc_attr( get_field( 'width' ) ) . '"';
+		$height_attr = 'height="' . esc_attr( get_field( 'height' ) ) . '"';
+		$iframe      = '<iframe ' . $width_attr . ' ' . $height_attr . ' ' . ' frameborder="0" style="border:0" src="' . $url . '" allowfullscreen></iframe>';
+		echo '<div class="gmap">' . $iframe . '</div>';
+	} else {
+		return false;
 	}
+}
 
 // GoogleMapストリートビューのURLを取得する関数
 	function get_gmap_sv_url( $width = 400, $height = 300 ) {
