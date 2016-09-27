840d51002c368523e8049989cc7b670a78458064
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-09-27 20:57:38 +0900

    :recycle: Improve card

--- template/card.php
+++ template/card.php
@@ -1,3 +1,7 @@
+<?php
+	$school = new School( get_field( 'schoolName' ) );
+	$year   = $school->get_post_year( get_the_ID() );
+?>
 <div class="card">
 	<?php
 		$img_url = get_gmap_sv_url( 385, 200 );
@@ -8,9 +12,9 @@
 		<?php endif; ?>
 	<div class="card-block">
 		<a href="<?php the_permalink(); ?>">
-			<h2 class="card-title"><?php the_field( 'name' ); ?></h2>
+			<h4 class="card-title"><?php the_field( 'name' ); ?></h4>
 		</a>
-		<h6 class="card-subtitle text-muted"><?php the_field( 'class' ); ?>&nbsp;<?php the_field( 'schoolName' ); ?></h6>
+		<h6 class="card-subtitle text-muted"><?php the_field( 'class' ); ?> <?php the_field( 'schoolName' ); ?></h6>
 		<div class="card-block">
 			<?php if ( is_bunkasai_during_open() ) : ?>
 				<p class="card-text text-info"><i class="fa fa-fw fa-flag" aria-hidden="true"></i>&nbsp;開催中</p>
@@ -21,43 +25,13 @@
 					<i class="fa fa-fw fa-clock-o" aria-hidden="true"></i>&nbsp;開催まで<?php echo esc_html( get_remaining_days() ); ?>日
 				</p>
 			<?php endif; ?>
-			<p class="card-text">
-				<span><i class="fa fa-fw fa-calendar"></i> 開催期間</span>
-				<?php if ( get_field( 'startDate' ) and get_field( 'endDate' ) ) : ?>
-					<span><?php the_field( 'startDate' ); ?>&nbsp;~&nbsp;<?php the_field( 'endDate' ); ?></span>
-				<?php elseif ( get_field( 'startDate' ) and ! get_field( 'endDate' ) ) : ?>
-					<span><?php the_field( 'startDate' ); ?></span>
-				<?php else : ?>
-					<span>不明</span>
-				<?php endif; ?>
-			</p>
-			<p class="card-text"><i class="fa fa-fw fa-info" aria-hidden="true"></i> 一般公開</p>
-			<?php if ( get_field( 'public_unknown' ) ) : ?>
-				<p class="card-text">不明</p>
-			<?php else : ?>
-				<?php if ( have_rows( 'public_open' ) ) : ?>
-					<?php while ( have_rows( 'public_open' ) ) : the_row(); ?>
-						<?php if ( get_sub_field( 'public_open_day' ) and get_sub_field( 'public_open_start_time' ) and get_sub_field( 'public_open_end_time' ) ) : ?>
-							<p class="card-text">
-								<?php the_sub_field( 'public_open_day' ); ?>&nbsp;<?php the_sub_field( 'public_open_start_time' ); ?>&nbsp;~&nbsp;<?php the_sub_field( 'public_open_end_time' ); ?>
-							</p>
-						<?php elseif ( get_sub_field( 'public_open_day' ) ) : ?>
-							<p class="card-text"><?php the_sub_field( 'public_open_day' ); ?></p>
-						<?php endif; ?>
-					<?php endwhile; ?>
-
-				<?php elseif ( get_field( 'publicStartDate' ) and get_field( 'publicEndDate' ) ) : ?>
-					<p class="card-text">
-						<?php the_field( 'publicStartDate' ); ?>&nbsp;~&nbsp;<?php the_field( 'publicEndDate' ); ?>
-					</p>
-				<?php elseif ( get_field( 'publicStartDate' ) and ! get_field( 'publicEndDate' ) ) : ?>
-					<p class="card-text">
-						<?php the_field( 'publicStartDate' ); ?>
-					</p>
-				<?php elseif ( ! have_rows( 'public_open' ) and ! get_field( 'publicStartDate' ) and ! get_field( 'publicEndDate' ) ) : ?>
-					<p class="card-text">なし</p>
-				<?php endif; ?>
-			<?php endif; ?>
+			<p class="card-text"><i class="fa fa-fw fa-calendar"></i>&nbsp;開催期間</p>
+			<p class="card-text"><?php echo esc_html( $school->the_event_date( $year ) ); ?></p>
+			<p class="card-text"><i class="fa fa-fw fa-info" aria-hidden="true"></i>&nbsp;一般公開</p>
+			<?php $event_public_open_date = $school->get_event_public_open_date( $year ); ?>
+			<?php foreach ( $event_public_open_date as $text ) : ?>
+				<p class="card-text"><?php echo esc_html( $text ); ?></p>
+			<?php endforeach; ?>
 		</div>
 	</div>
 	<div class="card-block text-xs-right">
