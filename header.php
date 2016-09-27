da841f14eb0b728f991cdbe7c855b177329e6b38
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-09-27 20:29:05 +0900

    :+1: Improve Navbar

--- header.php
+++ header.php
@@ -2,6 +2,6 @@
 <html <?php language_attributes(); ?>>
 <?php get_template_part( 'head' ); ?>
 <body <?php body_class(); ?>>
+<?php get_template_part( 'nav' ); ?>
 <!-- container -->
-	<div class="container">
-		<?php get_template_part( 'nav' ); ?>
+<div class="container">
