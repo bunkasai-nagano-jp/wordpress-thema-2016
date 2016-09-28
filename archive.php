64c01a2b3f4141c6c7bf09120506a1c3f5e48aa4
Sugiyama Masato <sugiyama@masato.nagano.jp>
2016-09-28 16:20:25 +0900

    :shirt: Fix Code Style

--- archive.php
+++ archive.php
@@ -1,12 +1,11 @@
 <?php get_header(); ?>
 <main>
 	<?php get_template_part( 'breadcrumb' ); ?>
-	<h1>
-	<?php if ( is_tag() ) {
-		single_tag_title();
-} elseif ( is_tax() ) {
-	single_term_title();
-} ?>の文化祭</h1>
+	<h1><?php if ( is_tag() ) :
+			single_tag_title();
+		elseif ( is_tax() ) :
+			single_term_title();
+		endif; ?>の文化祭</h1>
 	<?php get_template_part( 'itiran' ); ?>
 </main>
 <?php get_template_part( 'template/pagination' ); ?>
