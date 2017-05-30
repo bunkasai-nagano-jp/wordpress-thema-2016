e329f2e2a7829bedfe92f9232f88bb714284e936
Masato Sugiyama <sugiyama@masato.nagano.jp>
2017-05-30 11:13:38 +0900

    Remove adsense

--- footer.php
+++ footer.php
@@ -1,13 +1,6 @@
 </div>
 <!-- /container -->
 <!-- footer -->
-<?php if ( !wp_is_mobile() ) : ?>
-	<ins class="adsbygoogle"
-	     style="display:inline-block;width:970px;height:90px"
-	     data-ad-client="ca-pub-3116606223638769"
-	     data-ad-slot="3918021732"></ins>
-<?php endif; ?>
-
 <footer class="navbar navbar-light bg-faded">
 	<div class="container">
 		<nav>
