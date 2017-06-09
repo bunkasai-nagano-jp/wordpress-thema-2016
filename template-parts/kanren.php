ac39a2120b739c46aae991fdfc6ffc8931eba26b
Masato Sugiyama <sugiyama@masato.nagano.jp>
2017-06-09 13:56:35 +0900

    Fix code style

--- template-parts/kanren.php
+++ template-parts/kanren.php
@@ -1,16 +1,16 @@
 <?php
-	$relation_post = get_relation_post();
-if ( $relation_post->have_posts() ) :  ?>
-		<h2><i class="fa fa-th-list"></i>&nbsp;関連記事</h2>
-		<!-- .kanren-flex-container -->
-		<div class="kanren-flex-container">
-			<?php while ( $relation_post->have_posts() ) : $relation_post->the_post(); ?>
-				<!-- card -->
-				<?php get_template_part( 'template-parts/card' ); ?>
-				<!-- /card -->
-			<?php endwhile;
+$relation_post = get_relation_post();
+if ( $relation_post->have_posts() ) : ?>
+	<h2><i class="fa fa-th-list"></i>&nbsp;関連記事</h2>
+	<!-- .kanren-flex-container -->
+	<div class="kanren-flex-container">
+		<?php while ( $relation_post->have_posts() ) : $relation_post->the_post(); ?>
+			<!-- card -->
+			<?php get_template_part( 'template-parts/card' ); ?>
+			<!-- /card -->
+		<?php endwhile;
 		wp_reset_postdata();
-	?>
+		?>
 	</div>
 	<!-- /.kanren-flex-container -->
-	<?php endif;
+<?php endif;
