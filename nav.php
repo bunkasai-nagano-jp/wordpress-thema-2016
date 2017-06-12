f3a753be92ee97f32af1070a712bf4c16f67982a
Masato Sugiyama <sugiyama@masato.nagano.jp>
2017-06-12 23:30:40 +0900

    Update

--- nav.php
+++ nav.php
@@ -1,36 +1,31 @@
 <!-- nav -->
-<header class="navbar navbar-light bg-faded">
-	<div class="container">
-		<nav>
-			<div class="clearfix">
-				<button class="navbar-toggler pull-xs-right hidden-sm-up" type="button" data-toggle="collapse" data-target="#navbar-header" aria-controls="navbar-header" aria-expanded="false">
-					<i class="fa fa-bars" aria-hidden="true"></i>
-				</button>
-				<a class="navbar-brand hidden-sm-up" href="<?php echo esc_url( home_url() ); ?>/"><?php bloginfo( 'name' ); ?></a>
-			</div>
-			<div class="navbar-toggleable-xs collapse " id="navbar-header" aria-expanded="false">
-				<div class="nav navbar-nav">
-					<a class="nav-item nav-link" href="<?php echo esc_url( home_url() ); ?>/"><?php bloginfo( 'name' ); ?></a>
-					<?php
-						$defaults = [
-							'menu'           => 'navbar',
-							'container'      => false,
-							'echo'           => true,
-							'depth'          => 0,
-							'walker'         => new NavbarLinkList,
-							'theme_location' => 'navbar',
-							'items_wrap'     => '%3$s',
-						];
-						wp_nav_menu( $defaults ); ?>
-				</div>
-				<form action="https://www.google.com/cse" class="form-inline pull-sm-right" id="cse-search-box">
-					<input type="hidden" name="cx" value="000907066374336213658:lnnce0mnjry"/>
-					<input type="hidden" name="ie" value="UTF-8"/>
-					<input class="form-control" type="text" name="q" size="31"/>
-					<input class="btn btn-outline-success" type="submit" name="sa" value="Search"/>
-				</form>
-			</div>
-		</nav>
+<header class="navbar navbar-inverse bg-inverse navbar-toggleable-md">
+		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+		        data-target="#navbar-header" aria-controls="navbar-header" aria-expanded="false">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<a class="navbar-brand" href="<?php echo esc_url( home_url() ); ?>/"><?php bloginfo( 'name' ); ?></a>
+	<div class="collapse navbar-collapse" id="navbar-header" aria-expanded="false">
+		<div class="navbar-nav mr-auto">
+			<?php
+			$defaults = [
+				'menu'           => 'navbar',
+				'container'      => false,
+				'echo'           => true,
+				'depth'          => 0,
+				'walker'         => new NavbarLinkList,
+				'theme_location' => 'navbar',
+				'items_wrap'     => '%3$s',
+			];
+			wp_nav_menu( $defaults ); ?>
+		</div>
+		<form action="https://www.google.com/cse" class="form-inline my-2 my-md-0" id="cse-search-box">
+			<input type="hidden" name="cx" value="000907066374336213658:lnnce0mnjry"/>
+			<input type="hidden" name="ie" value="UTF-8"/>
+			<input class="form-control mr-sm-2" type="text" name="q" size="31"/>
+			<input class="btn btn-outline-success my-2 my-sm-0" type="submit" name="sa" value="Search"/>
+		</form>
 	</div>
 </header>
+
 <!-- /nav -->
