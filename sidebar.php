e1aaf5c619a1a14f4349452a635d6a2fe6e00227
Masato Sugiyama <sugiyama@masato.nagano.jp>
2017-07-01 19:28:17 +0900

    Update

--- sidebar.php
+++ sidebar.php
@@ -1,11 +1,31 @@
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-3116606223638769"
-     data-ad-slot="1067019734"
-     data-ad-format="auto"></ins>
-<script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-<div class="mb-3">
-	<?php relation_category_list(); ?>
+<div class="row">
+	<div class="col-md-12">
+		<ins class="adsbygoogle"
+		     style="display:block"
+		     data-ad-client="ca-pub-3116606223638769"
+		     data-ad-slot="1067019734"
+		     data-ad-format="auto"></ins>
+		<script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+		</script>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-12">
+		<?php relation_category_list(); ?>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-12">
+		<div class="mt-3">
+			<ins class="adsbygoogle"
+			     style="display:block"
+			     data-ad-format="autorelaxed"
+			     data-ad-client="ca-pub-3116606223638769"
+			     data-ad-slot="8828691732"></ins>
+			<script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
+		</div>
+	</div>
 </div>
